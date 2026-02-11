9dabf292c551bf94841ac2928addf67d8c2aaed1
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-11 20:04:06 +0530

    [378] [Time Beats: 19.86%] [Memory Beats: 93.14%] - LeetPush

--- Kth-Smallest-Element-in-a-Sorted-Matrix.java
+++ Kth-Smallest-Element-in-a-Sorted-Matrix.java
@@ -1,6 +1,6 @@
 1class Solution {
 2    public int kthSmallest(int[][] matrix, int k) {
-3         int rows = matrix.length;
+3        int rows = matrix.length;
 4         int cols = matrix[0].length;
 5
 6          int[] oneD = new int[rows * cols];
@@ -12,7 +12,15 @@
 12                    oneD[index++] = matrix[i][j];
 13              }
 14          }
-15          Arrays.sort(oneD);
-16          return oneD[k-1];
-17    }
-18}
+15         PriorityQueue<Integer>p=new PriorityQueue<>();
+16         for(int i=0;i<oneD.length;i++){
+17             p.add(oneD[i]);
+18         }
+19         int top=-1;
+20         while(k>0){
+21             top=p.poll();
+22             k--;
+23         }
+24       return top;
+25    }
+26}
