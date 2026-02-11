0eefcabd5c67a66d90906b1ea2044c4e7f2ab200
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-11 22:25:56 +0530

    [378] [Time Beats: 28.81%] [Memory Beats: 83.31%] - LeetPush

--- Kth-Smallest-Element-in-a-Sorted-Matrix.java
+++ Kth-Smallest-Element-in-a-Sorted-Matrix.java
@@ -1,11 +1,11 @@
 1class Solution {
 2
-3    static class Pair {
+3    static class Node {
 4        int value;
 5        int row;
 6        int col;
 7
-8        Pair(int value, int row, int col) {
+8        Node(int value, int row, int col) {
 9            this.value = value;
 10            this.row = row;
 11            this.col = col;
@@ -17,32 +17,39 @@
 17        int n = matrix.length;
 18
 19        // Min Heap based on value
-20        PriorityQueue<Pair> pq =
-21                new PriorityQueue<>((a, b) -> a.value - b.value);
+20        PriorityQueue<Node> pq =
+21                new PriorityQueue<>((a, b) -> Integer.compare(a.value, b.value));
 22
-23        // Insert first element of every row
-24        for (int i = 0; i < n; i++) {
-25            pq.add(new Pair(matrix[i][0], i, 0));
-26        }
-27
-28        int ans = 0;
-29
-30        while (k > 0) {
-31
-32            Pair current = pq.poll();
-33            ans = current.value;
+23        // Push first element
+24        pq.add(new Node(matrix[0][0], 0, 0));
+25
+26        // Mark visited
+27        matrix[0][0] = -1;
+28
+29        int ans = 0;
+30
+31        while (k-- > 0) {
+32
+33            Node element = pq.poll();
 34
-35            int r = current.row;
-36            int c = current.col;
-37
-38            // If next column exists
-39            if (c + 1 < n) {
-40                pq.add(new Pair(matrix[r][c + 1], r, c + 1));
-41            }
-42            k--;
-43        }
+35            ans = element.value;
+36            int i = element.row;
+37            int j = element.col;
+38
+39            // Down
+40            if (i + 1 < n && matrix[i + 1][j] != -1) {
+41                pq.add(new Node(matrix[i + 1][j], i + 1, j));
+42                matrix[i + 1][j] = -1;
+43            }
 44
-45        return ans;
-46    }
-47}
-48
+45            // Right
+46            if (j + 1 < n && matrix[i][j + 1] != -1) {
+47                pq.add(new Node(matrix[i][j + 1], i, j + 1));
+48                matrix[i][j + 1] = -1;
+49            }
+50        }
+51
+52        return ans;
+53    }
+54}
+55
