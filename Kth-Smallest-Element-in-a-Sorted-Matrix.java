607bb82393542f9c322b2f5edd423541c802f969
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-11 21:42:44 +0530

    [378] [Time Beats: 62.93%] [Memory Beats: 89.17%] - LeetPush

--- Kth-Smallest-Element-in-a-Sorted-Matrix.java
+++ Kth-Smallest-Element-in-a-Sorted-Matrix.java
@@ -1,26 +1,48 @@
 1class Solution {
-2    public int kthSmallest(int[][] matrix, int k) {
-3        int rows = matrix.length;
-4         int cols = matrix[0].length;
-5
-6          int[] oneD = new int[rows * cols];
+2
+3    static class Pair {
+4        int value;
+5        int row;
+6        int col;
 7
-8          int index = 0;
-9
-10          for (int i = 0; i < rows; i++) {
-11            for (int j = 0; j < cols; j++) {
-12                    oneD[index++] = matrix[i][j];
-13              }
-14          }
-15         PriorityQueue<Integer>p=new PriorityQueue<>();
-16         for(int i=0;i<oneD.length;i++){
-17             p.add(oneD[i]);
-18         }
-19         int top=-1;
-20         while(k>0){
-21             top=p.poll();
-22             k--;
-23         }
-24       return top;
-25    }
-26}
+8        Pair(int value, int row, int col) {
+9            this.value = value;
+10            this.row = row;
+11            this.col = col;
+12        }
+13    }
+14
+15    public int kthSmallest(int[][] matrix, int k) {
+16
+17        int n = matrix.length;
+18
+19        // Min Heap based on value
+20        PriorityQueue<Pair> pq =
+21                new PriorityQueue<>((a, b) -> a.value - b.value);
+22
+23        // Insert first element of every row
+24        for (int i = 0; i < n; i++) {
+25            pq.add(new Pair(matrix[i][0], i, 0));
+26        }
+27
+28        int ans = 0;
+29
+30        while (k > 0) {
+31
+32            Pair current = pq.poll();
+33            ans = current.value;
+34
+35            int r = current.row;
+36            int c = current.col;
+37
+38            // If next column exists
+39            if (c + 1 < n) {
+40                pq.add(new Pair(matrix[r][c + 1], r, c + 1));
+41            }
+42            k--;
+43        }
+44
+45        return ans;
+46    }
+47}
+48
