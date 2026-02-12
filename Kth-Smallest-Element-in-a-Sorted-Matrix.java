260f43acd930cc04326adbbd208a26c6624bf165
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-12 06:51:41 +0530

    [378] [Time Beats: 100.00%] [Memory Beats: 18.75%] - LeetPush

--- Kth-Smallest-Element-in-a-Sorted-Matrix.java
+++ Kth-Smallest-Element-in-a-Sorted-Matrix.java
@@ -1,55 +1,44 @@
 1class Solution {
 2
-3    static class Node {
-4        int value;
-5        int row;
-6        int col;
-7
-8        Node(int value, int row, int col) {
-9            this.value = value;
-10            this.row = row;
-11            this.col = col;
-12        }
-13    }
-14
-15    public int kthSmallest(int[][] matrix, int k) {
-16
-17        int n = matrix.length;
-18
-19        // Min Heap based on value
-20        PriorityQueue<Node> pq =
-21                new PriorityQueue<>((a, b) -> Integer.compare(a.value, b.value));
+3    // Count elements <= mid
+4    static int countLessEqual(int[][] mat, int n, int mid) {
+5
+6        int count = 0;
+7        int row = n - 1;
+8        int col = 0;
+9
+10        while (row >= 0 && col < n) {
+11
+12            if (mat[row][col] <= mid) {
+13                count += (row + 1);   // All elements above are also <= mid
+14                col++;               // Move right
+15            } else {
+16                row--;               // Move up
+17            }
+18        }
+19
+20        return count;
+21    }
 22
-23        // Push first element
-24        pq.add(new Node(matrix[0][0], 0, 0));
-25
-26        // Mark visited
-27        matrix[0][0] = -1;
-28
-29        int ans = 0;
-30
-31        while (k-- > 0) {
-32
-33            Node element = pq.poll();
-34
-35            ans = element.value;
-36            int i = element.row;
-37            int j = element.col;
-38
-39            // Down
-40            if (i + 1 < n && matrix[i + 1][j] != -1) {
-41                pq.add(new Node(matrix[i + 1][j], i + 1, j));
-42                matrix[i + 1][j] = -1;
-43            }
+23    public int kthSmallest(int[][] matrix, int k) {
+24      int n= matrix.length;
+25        int low = matrix[0][0];
+26        int high = matrix[n - 1][n - 1];
+27
+28        while (low <= high) { 
+29
+30            int mid = low + (high - low) / 2;
+31
+32            int count = countLessEqual(matrix, n, mid);
+33
+34            if (count >= k) {
+35                high = mid-1;      // Search left half 
+36            } else {
+37                low = mid + 1;   // Search right half
+38            }
+39        }
+40
+41        return low;   // kth smallest element
+42    }
+43}
 44
-45            // Right
-46            if (j + 1 < n && matrix[i][j + 1] != -1) {
-47                pq.add(new Node(matrix[i][j + 1], i, j + 1));
-48                matrix[i][j + 1] = -1;
-49            }
-50        }
-51
-52        return ans;
-53    }
-54}
-55
