6685edb3c1967c5435b9eafd1d484eb96a6b9cf5
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-14 00:45:45 +0530

    [347] [Time Beats: 77.82%] [Memory Beats: 90.78%] - LeetPush

--- Top-K-Frequent-Elements.java
+++ Top-K-Frequent-Elements.java
@@ -1,21 +1,34 @@
 1class Solution {
-2    public int[] topKFrequent(int[] nums, int k) {
-3        HashMap<Integer,Integer>map=new HashMap<>();
-4        for(int i=0;i<nums.length;i++){
-5            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-6        }
-7        //convert map to list
-8        List<Map.Entry<Integer,Integer>>list=new ArrayList<>(map.entrySet());
-9    
-10     // Step 3: Sort by frequency (descending)
-11        Collections.sort(list, (a, b) -> b.getValue() - a.getValue());
-12
-13        // Step 4: Pick top k
-14        int[] ans = new int[k];
-15        for (int i = 0; i < k; i++) {
-16            ans[i] = list.get(i).getKey();
-17        }
-18
-19        return ans;
-20}
-21}
+2    static class Pair{
+3        int freq;
+4        int val;
+5        Pair(int freq,int val){
+6            this.freq=freq;
+7            this.val=val;
+8        }
+9    }
+10    public int[] topKFrequent(int[] nums, int k) {
+11        HashMap<Integer,Integer>map=new HashMap<>();
+12        for(int i=0;i<nums.length;i++){
+13            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+14        }
+15      
+16     // Iterate over map entries
+17       PriorityQueue<Pair> p = new PriorityQueue<>((a, b) -> a.freq - b.freq);
+18        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+19            p.add(new Pair(e.getValue(), e.getKey()));
+20            if (p.size() > k) {
+21                p.poll();
+22            }
+23        }
+24
+25        int[] ans = new int[k];
+26        int i = 0;
+27        while (!p.isEmpty()) {
+28            ans[i] = p.poll().val;
+29            i++;
+30        }
+31
+32        return ans;
+33}
+34}
