21652c74022afb0a8759c8ae7dbb8126d8b4a74d
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-18 22:34:37 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 75.87%] - LeetPush

--- House-Robber.java
+++ House-Robber.java
@@ -1,16 +1,20 @@
 1class Solution {
-2    int[]  dp;
-3     public int rob(int[] nums) {
-4        dp=new int[nums.length];
-5        Arrays.fill(dp,-1);
-6     return robber(nums.length-1,nums);
-7}
-8int robber(int idx,int[] nums){
-9    if(idx==0) return nums[0];
-10    if(idx<0) return 0;
-11     if(dp[idx]!=-1) return dp[idx];
-12    int pick=nums[idx]+robber(idx-2,nums);
-13    int nonpick=0+robber(idx-1,nums);
-14    return dp[idx]=Math.max(pick,nonpick);
-15}
-16}
+2     public int rob(int[] nums) {
+3      int n = nums.length;
+4        
+5        if (n == 0) return 0;
+6        if (n == 1) return nums[0];
+7
+8        int[] dp = new int[n];
+9        dp[0] = nums[0];
+10        for(int i=1;i<n;i++){
+11        int pick=nums[i];
+12        if(i>1){
+13         pick+=dp[i-2];
+14        }
+15        int nonpick=0+dp[i-1];
+16        dp[i]=Math.max(pick,nonpick);
+17        }
+18        return dp[n-1];
+19     }
+20}
