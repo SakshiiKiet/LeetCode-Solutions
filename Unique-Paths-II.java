6d6a94500dedb03a8cf4922b0659fb743ced3b6c
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-21 13:46:54 +0530

    [63] [Time Beats: 100.00%] [Memory Beats: 44.47%] - LeetPush

--- Unique-Paths-II.java
+++ Unique-Paths-II.java
@@ -2,22 +2,22 @@
 2    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
 3        int m=obstacleGrid.length;
 4        int n=obstacleGrid[0].length;
-5       int[] prev=new int[n];
-6           for(int i=0;i<m;i++){
-7            int[] temp = new int[n];
-8            for(int j=0;j<n;j++){
-9                if(obstacleGrid[i][j]==1){
-10                  temp[j]=0;
-11               }else if(i==0 && j==0){
-12                temp[j]=1;
-13                }else{
-14                 int up = (i > 0) ? prev[j] : 0;
-15                    int left = (j > 0) ? temp[j - 1] : 0;
-16                    temp[j] = up + left;
-17               }
-18          }
-19        prev=temp;
-20        }
-21        return prev[n-1];     
+5       int[] dp = new int[n];
+6
+7        // Starting point
+8         dp[0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+9
+10        for (int i = 0; i < m; i++) {
+11            for (int j = 0; j < n; j++) {
+12                if (obstacleGrid[i][j] == 1) {
+13                    dp[j] = 0;              // blocked cell
+14                }
+15                else if (j > 0) {
+16                    dp[j] = dp[j] + dp[j - 1]; // up + left
+17                }
+18            }
+19        }
+20
+21        return dp[n - 1]; 
 22  }
 23}
