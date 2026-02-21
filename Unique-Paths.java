1d10c83811d9fd2130a9632b47ba91ffcc6cb4a8
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-21 07:58:31 +0530

    [62] [Time Beats: 14.35%] [Memory Beats: 66.12%] - LeetPush

--- Unique-Paths.java
+++ Unique-Paths.java
@@ -1,18 +1,24 @@
 1class Solution {
-2    int dp[][];
-3    public int uniquePaths(int m, int n) {
-4        dp=new int[m][n];
-5        for(int[] row:dp){
-6            Arrays.fill(row,-1);
-7        }
-8       return path(m-1,n-1);
-9    }
-10    int path(int i,int j){
-11        if(i==0 && j==0) return 1;
-12       if(i<0 || j<0) return 0;
-13       if(dp[i][j]!=-1) return dp[i][j];
-14       int up=path(i-1,j);
-15       int left=path(i,j-1);
-16       return dp[i][j]=up+left;
-17    }
-18}
+2    public int uniquePaths(int m, int n) {
+3
+4        int[][] dp = new int[m][n];
+5        dp[0][0] = 1;
+6
+7        for (int i = 0; i < m; i++) {
+8            for (int j = 0; j < n; j++) {
+9
+10                if (i == 0 && j == 0) continue;
+11
+12                int up = 0;
+13                int left = 0;
+14
+15                if (i > 0) up = dp[i - 1][j];
+16                if (j > 0) left = dp[i][j - 1];
+17
+18                dp[i][j] = up + left;
+19            }
+20        }
+21
+22        return dp[m - 1][n - 1];
+23    }
+24}
