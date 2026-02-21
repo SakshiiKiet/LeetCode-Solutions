48969a44573a54d7a5916679b2dbec87ac8e656f
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-21 13:06:51 +0530

    [63] [Time Beats: 100.00%] [Memory Beats: 9.96%] - LeetPush

--- Unique-Paths-II.java
+++ Unique-Paths-II.java
@@ -1,21 +1,27 @@
 1class Solution {
-2    int[][] dp;
-3    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-4        int m=obstacleGrid.length;
-5        int n=obstacleGrid[0].length;
-6         dp=new int[m][n];
-7         for(int[] row:dp){
-8            Arrays.fill(row,-1);
-9        }
-10        return path(m-1,n-1,obstacleGrid);
-11    }
-12    int path(int i,int j,int[][] obstacleGrid){
-13        if(i>=0 && j>=0 && obstacleGrid[i][j]==1) return 0;
-14        if(i==0 && j==0) return 1;
-15        if(i<0 || j<0) return 0;
-16        if(dp[i][j]!=-1) return dp[i][j];
-17        int up=path(i-1,j,obstacleGrid);
-18        int left=path(i,j-1,obstacleGrid);
-19        return dp[i][j]=up+left;
-20    }
-21}
+2    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+3        int m=obstacleGrid.length;
+4        int n=obstacleGrid[0].length;
+5       int[][] dp=new int[m][n];
+6           for(int i=0;i<m;i++){
+7            for(int j=0;j<n;j++){
+8                if(obstacleGrid[i][j]==1){
+9                  dp[i][j]=0; // for obstacle
+10                } 
+11                else if(i==0 && j==0){
+12                 dp[i][j]=1;
+13                }else{
+14                   int up = 0;
+15                int left = 0;
+16
+17                if (i > 0) up = dp[i - 1][j];
+18                if (j > 0) left = dp[i][j - 1];
+19
+20                dp[i][j] = up + left;
+21
+22                }
+23            }
+24           }
+25           return dp[m-1][n-1];
+26    }
+27}
