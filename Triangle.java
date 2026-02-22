21e1b6b1e6db95030e5f2518838cf669c8849d13
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-22 20:28:42 +0530

    [120] [Time Beats: 40.38%] [Memory Beats: 64.61%] - LeetPush

--- Triangle.java
+++ Triangle.java
@@ -1,18 +1,19 @@
 1class Solution {
-2    int[][] dp;
-3    public int minimumTotal(List<List<Integer>> triangle) {
-4        int n=triangle.size();
-5        dp=new int[n][n];
-6        for(int j=0;j<n;j++){
-7            dp[n-1][j]=triangle.get(n-1).get(j);
-8        }
-9        for(int i=n-2;i>=0;i--){
+2    public int minimumTotal(List<List<Integer>> triangle) {
+3        int n=triangle.size();
+4        int[] prev=new int[n];
+5        for(int j=0;j<n;j++){
+6            prev[j]=triangle.get(n-1).get(j);
+7        }
+8        for(int i=n-2;i>=0;i--){
+9            int[] curr=new int[n];
 10            for(int j=i;j>=0;j--){
-11                int d=triangle.get(i).get(j)+dp[i+1][j];
-12                int dg=triangle.get(i).get(j)+dp[i+1][j+1];
-13                dp[i][j]=Math.min(d,dg);
+11                int d=triangle.get(i).get(j)+prev[j];
+12                int dg=triangle.get(i).get(j)+prev[j+1];
+13                curr[j]=Math.min(d,dg);
 14            }
-15        }
-16        return dp[0][0];
-17    }
-18}
+15            prev=curr;
+16        }
+17        return prev[0];
+18    }
+19}
