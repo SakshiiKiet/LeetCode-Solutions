f9df80cd0257a94c03ea94aa4ca8080ed04093f1
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-22 07:15:08 +0530

    [64] [Time Beats: 10.45%] [Memory Beats: 72.27%] - LeetPush

--- Minimum-Path-Sum.java
+++ Minimum-Path-Sum.java
@@ -1,22 +1,22 @@
 1class Solution {
-2    int[][] dp;
-3
-4    public int minPathSum(int[][] grid) {
-5        int m = grid.length;
-6        int n = grid[0].length;
-7        dp = new int[m][n];
-8        for (int[] row : dp) Arrays.fill(row, -1);
-9        return path(m - 1, n - 1, grid);
-10    }
-11
-12    int path(int i, int j, int[][] grid) {
-13        if (i == 0 && j == 0) return grid[0][0];
-14        if (i < 0 || j < 0) return Integer.MAX_VALUE / 2;
-15        if (dp[i][j] != -1) return dp[i][j];
-16
-17        int up = grid[i][j] + path(i - 1, j, grid);
-18        int left = grid[i][j] + path(i, j - 1, grid);
-19
-20        return dp[i][j] = Math.min(up, left);
+2    public int minPathSum(int[][] grid) {
+3        int m = grid.length;
+4        int n = grid[0].length;
+5        int[][] dp = new int[m][n];
+6       for(int i=0;i<m;i++){
+7        for(int j=0;j<n;j++){
+8            if(i==0 && j==0){
+9                dp[i][j]=grid[0][0];
+10            }else{
+11                 int up = Integer.MAX_VALUE;
+12                    int left = Integer.MAX_VALUE;
+13
+14                if (i > 0) up = grid[i][j] +dp[i - 1][j];
+15                if (j > 0) left = grid[i][j] +dp[i][j - 1];
+16                dp[i][j]=Math.min(up,left);
+17            }
+18        }
+19       }
+20        return dp[m-1][n-1];
 21    }
 22}
