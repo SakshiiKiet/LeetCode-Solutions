e232dea5b00c8641473a7eca403a841f5a0450cd
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-22 07:31:32 +0530

    [64] [Time Beats: 25.62%] [Memory Beats: 38.11%] - LeetPush

--- Minimum-Path-Sum.java
+++ Minimum-Path-Sum.java
@@ -2,21 +2,23 @@
 2    public int minPathSum(int[][] grid) {
 3        int m = grid.length;
 4        int n = grid[0].length;
-5        int[][] dp = new int[m][n];
+5        int[] prev = new int[n];
 6       for(int i=0;i<m;i++){
-7        for(int j=0;j<n;j++){
-8            if(i==0 && j==0){
-9                dp[i][j]=grid[0][0];
-10            }else{
-11                 int up = Integer.MAX_VALUE;
-12                    int left = Integer.MAX_VALUE;
-13
-14                if (i > 0) up = grid[i][j] +dp[i - 1][j];
-15                if (j > 0) left = grid[i][j] +dp[i][j - 1];
-16                dp[i][j]=Math.min(up,left);
-17            }
-18        }
-19       }
-20        return dp[m-1][n-1];
-21    }
-22}
+7          int[] temp=new int[n];
+8        for(int j=0;j<n;j++){
+9            if(i==0 && j==0){
+10                temp[j]=grid[0][0];
+11            }else{
+12                 int up = Integer.MAX_VALUE;
+13                    int left = Integer.MAX_VALUE;
+14
+15                if (i > 0) up = grid[i][j] +prev[j];
+16                if (j > 0) left = grid[i][j] +temp[j - 1];
+17                temp[j]=Math.min(up,left);
+18            }
+19        }
+20        prev=temp;
+21       }
+22        return prev[n-1];
+23    }
+24}
