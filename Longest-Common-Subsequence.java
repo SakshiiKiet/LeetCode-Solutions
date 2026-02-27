8e0515181809fde860a703da0305397d91fea6ee
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-27 12:15:23 +0530

    [1143] [Time Beats: 89.85%] [Memory Beats: 91.51%] - LeetPush

--- Longest-Common-Subsequence.java
+++ Longest-Common-Subsequence.java
@@ -2,25 +2,25 @@
 2    public int longestCommonSubsequence(String text1, String text2) {
 3        int m = text1.length();
 4        int n = text2.length();
-5        int[][] dp=new int[m+1][n+1];
-6        for(int i=0;i<m;i++){
-7            dp[i][0]=0;
-8        }                          //Base Case    
-9        for(int j=0;j<n;j++){
-10            dp[0][j]=0;
-11        }
-12
-13        for(int i=1;i<=m;i++){
-14            for(int j=1;j<=n;j++){
-15        if(text1.charAt(i-1)==text2.charAt(j-1)){
-16             dp[i][j]= 1+dp[i-1][j-1];
-17        }else{
-18              dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+5        int[] prev=new int[n+1];
+6         //Base Case    
+7        for(int j=0;j<n;j++){
+8            prev[j]=0;
+9        }
+10
+11        for(int i=1;i<=m;i++){
+12             int[] curr=new int[n+1];   
+13            for(int j=1;j<=n;j++){
+14        if(text1.charAt(i-1)==text2.charAt(j-1)){
+15             curr[j]= 1+prev[j-1];
+16        }else{
+17              curr[j]=Math.max(prev[j],curr[j-1]); //dp[i-1][j],    prev[j]
+18                                                    //dp[i][j-1],     curr[j-1]
 19            }
 20        }
-21        
+21        prev=curr;
 22        }
-23        return dp[m][n];
+23        return prev[n];
 24    }
 25}
 26
