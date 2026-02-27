5025ac49fade418dbcdb601feb653be22397578a
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-27 12:12:34 +0530

    [1143] [Time Beats: 93.86%] [Memory Beats: 51.18%] - LeetPush

--- Longest-Common-Subsequence.java
+++ Longest-Common-Subsequence.java
@@ -1,23 +1,26 @@
 1class Solution {
-2    int[][] dp;
-3    public int longestCommonSubsequence(String text1, String text2) {
-4        int m = text1.length();
-5        int n = text2.length();
-6        dp=new int[m][n];
-7        for(int[] rows:dp){
-8            Arrays.fill(rows,-1);
-9        }
-10    return longest(m-1,n-1,text1,text2);
-11        
-12    }
-13    int longest(int i,int j,String text1, String text2){
-14        if(i<0 || j<0) return 0;
-15        if(dp[i][j]!=-1) return dp[i][j];
-16        if(text1.charAt(i)==text2.charAt(j)){
-17            return dp[i][j]= 1+longest(i-1,j-1,text1,text2);
-18        }else{
-19             return dp[i][j]=Math.max(longest(i-1,j,text1,text2),longest(i,j-1,text1,text2));
+2    public int longestCommonSubsequence(String text1, String text2) {
+3        int m = text1.length();
+4        int n = text2.length();
+5        int[][] dp=new int[m+1][n+1];
+6        for(int i=0;i<m;i++){
+7            dp[i][0]=0;
+8        }                          //Base Case    
+9        for(int j=0;j<n;j++){
+10            dp[0][j]=0;
+11        }
+12
+13        for(int i=1;i<=m;i++){
+14            for(int j=1;j<=n;j++){
+15        if(text1.charAt(i-1)==text2.charAt(j-1)){
+16             dp[i][j]= 1+dp[i-1][j-1];
+17        }else{
+18              dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+19            }
 20        }
-21    }
-22}
-23
+21        
+22        }
+23        return dp[m][n];
+24    }
+25}
+26
