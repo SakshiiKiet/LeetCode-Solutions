6735bd427c954ebd688ece8ac7789760533d75c7
Sakshi Maan <sakshimaan2004@gmail.com>
2026-02-27 15:50:52 +0530

    [516] [Time Beats: 43.43%] [Memory Beats: 67.80%] - LeetPush

--- Longest-Palindromic-Subsequence.java
+++ Longest-Palindromic-Subsequence.java
@@ -6,24 +6,24 @@
 6    public int longestCommonSubsequence(String text1, String text2) {
 7        int m = text1.length();
 8        int n = text2.length();
-9        int[] prev=new int[n+1];
-10         //Base Case    
-11        for(int j=0;j<n;j++){
-12            prev[j]=0;
-13        }
-14
-15        for(int i=1;i<=m;i++){
-16             int[] curr=new int[n+1];   
-17            for(int j=1;j<=n;j++){
-18        if(text1.charAt(i-1)==text2.charAt(j-1)){
-19             curr[j]= 1+prev[j-1];
-20        }else{
-21              curr[j]=Math.max(prev[j],curr[j-1]); //dp[i-1][j],    prev[j]
-22                                                    //dp[i][j-1],     curr[j-1]
+9        int[][] dp=new int[m+1][n+1];
+10        for(int i=0;i<m;i++){
+11            dp[i][0]=0;
+12        }                          //Base Case    
+13        for(int j=0;j<n;j++){
+14            dp[0][j]=0;
+15        }
+16
+17        for(int i=1;i<=m;i++){
+18            for(int j=1;j<=n;j++){
+19        if(text1.charAt(i-1)==text2.charAt(j-1)){
+20             dp[i][j]= 1+dp[i-1][j-1];
+21        }else{
+22              dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
 23            }
 24        }
-25        prev=curr;
+25        
 26        }
-27        return prev[n];
+27        return dp[m][n];
 28}
 29}
