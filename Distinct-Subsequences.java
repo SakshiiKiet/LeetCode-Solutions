3613d59a0bef8f3a7865fc5884cdd90fdb8caec0
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-02 00:08:14 +0530

    [115] [Time Beats: 11.65%] [Memory Beats: 36.74%] - LeetPush

---
+++ Distinct-Subsequences.java
@@ -0,0 +1,24 @@
+1class Solution {
+2    int[][] dp;
+3    public int numDistinct(String s, String t) {
+4        int n=s.length();
+5        int m=t.length();
+6        dp=new int[n][m];
+7        for (int[] rows : dp) {
+8            Arrays.fill(rows, -1);
+9        }
+10        return ways(n-1,m-1,s,t);
+11    }
+12    int ways(int i,int j,String s,String t){
+13        if(j<0) return 1;
+14        if(i<0) return 0;
+15        if(dp[i][j]!=-1) return dp[i][j];
+16        if(s.charAt(i)==t.charAt(j)){
+17            dp[i][j]= ways(i-1,j-1,s,t)+ways(i-1,j,s,t);
+18        }else{
+19           dp[i][j]= ways(i-1,j,s,t);  
+20        }
+21        return dp[i][j];
+22    }
+23    
+24}
