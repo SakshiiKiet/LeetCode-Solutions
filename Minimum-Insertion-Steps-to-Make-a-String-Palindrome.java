d1aa9f3db70a86d31ed240b97c068ce39eaf09bc
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-01 18:48:32 +0530

    [1312] [Time Beats: 61.41%] [Memory Beats: 18.71%] - LeetPush

--- Minimum-Insertion-Steps-to-Make-a-String-Palindrome.java
+++ Minimum-Insertion-Steps-to-Make-a-String-Palindrome.java
@@ -6,27 +6,27 @@
 6        String rev = new StringBuilder(s).reverse().toString();
 7        return longestCommonSubsequence(s,rev);
 8    }
-9     public int longestCommonSubsequence(String text1, String text2) {
+9    public int longestCommonSubsequence(String text1, String text2) {
 10        int m = text1.length();
 11        int n = text2.length();
-12        int[] prev=new int[n+1];
-13         //Base Case    
-14        for(int j=0;j<n;j++){
-15            prev[j]=0;
-16        }
-17
-18        for(int i=1;i<=m;i++){
-19             int[] curr=new int[n+1];   
-20            for(int j=1;j<=n;j++){
-21        if(text1.charAt(i-1)==text2.charAt(j-1)){
-22             curr[j]= 1+prev[j-1];
-23        }else{
-24              curr[j]=Math.max(prev[j],curr[j-1]); //dp[i-1][j],    prev[j]
-25                                                    //dp[i][j-1],     curr[j-1]
+12        int[][] dp=new int[m+1][n+1];
+13        for(int i=0;i<m;i++){
+14            dp[i][0]=0;
+15        }                          //Base Case    
+16        for(int j=0;j<n;j++){
+17            dp[0][j]=0;
+18        }
+19
+20        for(int i=1;i<=m;i++){
+21            for(int j=1;j<=n;j++){
+22        if(text1.charAt(i-1)==text2.charAt(j-1)){
+23             dp[i][j]= 1+dp[i-1][j-1];
+24        }else{
+25              dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
 26            }
 27        }
-28        prev=curr;
+28        
 29        }
-30        return prev[n];
+30        return dp[m][n];
 31}
 32}
