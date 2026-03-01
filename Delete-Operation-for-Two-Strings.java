380f7817f4a822ee7dfae21994ac2db31bf800b3
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-01 19:39:05 +0530

    [583] [Time Beats: 72.25%] [Memory Beats: 85.30%] - LeetPush

--- Delete-Operation-for-Two-Strings.java
+++ Delete-Operation-for-Two-Strings.java
@@ -1,27 +1,31 @@
 1class Solution {
-2    int[][]dp;
-3    public int minDistance(String word1, String word2) {
-4        int n=word1.length();
-5        int m=word2.length();
-6        return n+m-2*longestCommonSubsequence( word1,  word2);
-7    }
-8   public int longestCommonSubsequence(String text1, String text2) {
-9        int m = text1.length();
-10        int n = text2.length();
-11        dp=new int[m][n];
-12        for(int[] rows:dp){
-13            Arrays.fill(rows,-1);
-14        }
-15    return longest(m-1,n-1,text1,text2);
-16        
-17    }
-18    int longest(int i,int j,String text1, String text2){
-19        if(i<0 || j<0) return 0;
-20        if(dp[i][j]!=-1) return dp[i][j];
-21        if(text1.charAt(i)==text2.charAt(j)){
-22            return dp[i][j]= 1+longest(i-1,j-1,text1,text2);
-23        }else{
-24             return dp[i][j]=Math.max(longest(i-1,j,text1,text2),longest(i,j-1,text1,text2));
+2    public int minDistance(String word1, String word2) {
+3        int n=word1.length();
+4        int m=word2.length();
+5        return n+m-2*longestCommonSubsequence( word1,  word2);
+6    }
+7    public int longestCommonSubsequence(String text1, String text2) {
+8        int m = text1.length();
+9        int n = text2.length();
+10        int[][] dp=new int[m+1][n+1];
+11        for(int i=0;i<m;i++){
+12            dp[i][0]=0;
+13        }                          //Base Case    
+14        for(int j=0;j<n;j++){
+15            dp[0][j]=0;
+16        }
+17
+18        for(int i=1;i<=m;i++){
+19            for(int j=1;j<=n;j++){
+20        if(text1.charAt(i-1)==text2.charAt(j-1)){
+21             dp[i][j]= 1+dp[i-1][j-1];
+22        }else{
+23              dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+24            }
 25        }
-26    }
-27}
+26        
+27        }
+28        return dp[m][n];
+29    }
+30}
+31
