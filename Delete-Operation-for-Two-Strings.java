56da72e3929bac3a03193e27c7e2a5a644bf42aa
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-01 19:40:17 +0530

    [583] [Time Beats: 94.81%] [Memory Beats: 48.47%] - LeetPush

--- Delete-Operation-for-Two-Strings.java
+++ Delete-Operation-for-Two-Strings.java
@@ -7,25 +7,24 @@
 7    public int longestCommonSubsequence(String text1, String text2) {
 8        int m = text1.length();
 9        int n = text2.length();
-10        int[][] dp=new int[m+1][n+1];
-11        for(int i=0;i<m;i++){
-12            dp[i][0]=0;
-13        }                          //Base Case    
-14        for(int j=0;j<n;j++){
-15            dp[0][j]=0;
-16        }
-17
-18        for(int i=1;i<=m;i++){
-19            for(int j=1;j<=n;j++){
-20        if(text1.charAt(i-1)==text2.charAt(j-1)){
-21             dp[i][j]= 1+dp[i-1][j-1];
-22        }else{
-23              dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+10        int[] prev=new int[n+1];
+11         //Base Case    
+12        for(int j=0;j<n;j++){
+13            prev[j]=0;
+14        }
+15
+16        for(int i=1;i<=m;i++){
+17             int[] curr=new int[n+1];   
+18            for(int j=1;j<=n;j++){
+19        if(text1.charAt(i-1)==text2.charAt(j-1)){
+20             curr[j]= 1+prev[j-1];
+21        }else{
+22              curr[j]=Math.max(prev[j],curr[j-1]); //dp[i-1][j],    prev[j]
+23                                                    //dp[i][j-1],     curr[j-1]
 24            }
 25        }
-26        
+26        prev=curr;
 27        }
-28        return dp[m][n];
+28        return prev[n];
 29    }
 30}
-31
