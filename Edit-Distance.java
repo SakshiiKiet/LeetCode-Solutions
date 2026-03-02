e0fba41460014181479faaa293cf36a98cf692b0
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-02 09:12:31 +0530

    [72] [Time Beats: 77.01%] [Memory Beats: 93.60%] - LeetPush

--- Edit-Distance.java
+++ Edit-Distance.java
@@ -2,24 +2,24 @@
 2    public int minDistance(String word1, String word2) {
 3         int n=word1.length();
 4         int m=word2.length();
-5         int[][] dp=new int[n+1][m+1];
-6         
-7        for(int j=0;j<=m;j++){
-8            dp[0][j]=j;
-9        }
-10        for(int i=0;i<=n;i++){
-11            dp[i][0]=i;
-12        }
-13        for(int i=1;i<=n;i++){
-14            for(int j=1;j<=m;j++){
-15               if(word1.charAt(i-1)==word2.charAt(j-1)){
-16                 dp[i][j]=0 + dp[i-1][j-1];
-17               } 
-18               else{
-19                 dp[i][j]=1+Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));
-20               }  
-21            }
+5         int[] prev=new int[m+1];
+6         for(int j=0;j<=m;j++){
+7            prev[j]=j;
+8         }
+9        
+10        for(int i=1;i<=n;i++){
+11            int[] curr=new int[m+1];
+12            curr[0]=i;
+13            for(int j=1;j<=m;j++){
+14               if(word1.charAt(i-1)==word2.charAt(j-1)){
+15                 curr[j]=0 + prev[j-1];
+16               } 
+17               else{
+18                 curr[j]=1+Math.min(curr[j-1],Math.min(prev[j],prev[j-1]));
+19               }  
+20            }
+21            prev=curr;
 22        }
-23       return dp[n][m];
+23       return prev[m];
 24    }
 25}
