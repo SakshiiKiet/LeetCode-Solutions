939e42f103e09ba86ccd81f148de0ed23ece4d7d
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-03 00:13:10 +0530

    [122] [Time Beats: 100.00%] [Memory Beats: 31.27%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock-II.java
+++ Best-Time-to-Buy-and-Sell-Stock-II.java
@@ -1,22 +1,15 @@
 1class Solution {
 2    public int maxProfit(int[] prices) {
 3        int n=prices.length;
-4      int[][]  dp=new int[n+1][2];
-5       
-6        dp[n][0]=dp[n][1]=0;
+4      
+5       int currbuy,currNotbuy,aheadbuy,aheadNotbuy;
+6        aheadbuy=aheadNotbuy=0;
 7        for(int i=n-1;i>=0;i--){
-8            for(int buy=1;buy>=0;buy--){
-9                 int profit=0;
-10        if(buy==1){
-11        profit=Math.max((-prices[i]+dp[i+1][0]), //buy
-12                        (0+dp[i+1][1]));          //not buy       
-13     }else{
-14        profit=Math.max((prices[i]+dp[i+1][1]), //sell
-15                        (0+dp[i+1][0]));          //not sell      
-16              }
-17              dp[i][buy]=profit;
-18            }
-19        }
-20        return dp[0][1];
-21    }
-22}
+8            currNotbuy=Math.max(prices[i]+aheadbuy,0+aheadNotbuy);
+9            currbuy=Math.max(-prices[i]+aheadNotbuy,0+aheadbuy);
+10            aheadbuy=currbuy;
+11            aheadNotbuy=currNotbuy;
+12        }
+13        return aheadbuy;
+14    }
+15}
