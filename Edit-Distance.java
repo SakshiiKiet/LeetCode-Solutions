a90bfd0f3afe70052c59c7d1275af583936c6621
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-02 08:47:48 +0530

    [72] [Time Beats: 97.76%] [Memory Beats: 5.36%] - LeetPush

--- Edit-Distance.java
+++ Edit-Distance.java
@@ -3,17 +3,17 @@
 3    public int minDistance(String word1, String word2) {
 4         int n=word1.length();
 5         int m=word2.length();
-6         dp=new int[n+1][m+1];
+6         dp=new int[n][m];
 7         for(int[] rows:dp){
 8            Arrays.fill(rows,-1);
 9         }
-10        return operations(n,m,word1,word2);
+10        return operations(n-1,m-1,word1,word2);
 11    }
 12    int operations(int i,int j,String s1, String s2){
-13        if(i==0) return j;
-14        if(j==0) return i;
+13        if(i<0) return j+1;
+14        if(j<0) return i+1;
 15        if(dp[i][j]!=-1) return dp[i][j];
-16        if(s1.charAt(i-1)==s2.charAt(j-1)){
+16        if(s1.charAt(i)==s2.charAt(j)){
 17          return dp[i][j]=0 + operations(i-1,j-1,s1,s2);
 18        } 
 19        return dp[i][j]=1+Math.min(operations(i,j-1,s1,s2),Math.min(operations(i-1,j,s1,s2),operations(i-1,j-1,s1,s2)));
