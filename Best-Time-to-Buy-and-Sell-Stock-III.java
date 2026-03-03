a478006dfe629e48408177d2a217b5e2776c86b4
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-03 12:26:30 +0530

    [123] [Time Beats: 48.43%] [Memory Beats: 45.92%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock-III.java
+++ Best-Time-to-Buy-and-Sell-Stock-III.java
@@ -1,23 +1,23 @@
 1class Solution {
 2    public int maxProfit(int[] prices) {
 3        int n = prices.length;
-4        int[][] dp=new int[n+1][4+1];
-5        for(int trans=0;trans<4;trans++){
-6            dp[n][trans]=0;
+4        int[] ahead=new int[4+1];
+5        for(int trans=0;trans<=4;trans++){
+6            ahead[trans]=0;
 7        }
-8        for(int i=1;i<=n;i++){
-9            dp[i][4]=0;
-10        }
-11        for(int i=n-1;i>=0;i--){
-12          for(int trans=3;trans>=0;trans--){
-13             if(trans%2==0){ //buy
-14          dp[i][trans]=Math.max(-prices[i]+dp[i+1][trans+1],0+dp[i+1][trans]);
-15        }else{
-16           dp[i][trans]=Math.max(prices[i]+dp[i+1][trans+1],0+dp[i+1][trans]) ;
-17        }
-18          }
+8        for(int i=n-1;i>=0;i--){
+9            int[] curr=new int[4+1];
+10            curr[4]=0;
+11          for(int trans=3;trans>=0;trans--){
+12             if(trans%2==0){ //buy
+13          curr[trans]=Math.max(-prices[i]+ahead[trans+1],0+ahead[trans]);
+14        }else{
+15           curr[trans]=Math.max(prices[i]+ahead[trans+1],0+ahead[trans]) ;
+16        }
+17          }
+18          ahead=curr;
 19        }
 20       
-21        return dp[0][0];
+21        return ahead[0];
 22    }
 23}
