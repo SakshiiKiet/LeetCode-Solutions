f39021013bf402637873c710c765a93d4d9867ed
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-03 08:57:26 +0530

    [123] [Time Beats: 49.75%] [Memory Beats: 79.77%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock-III.java
+++ Best-Time-to-Buy-and-Sell-Stock-III.java
@@ -1,33 +1,32 @@
 1class Solution {
 2    public int maxProfit(int[] prices) {
-3        int n=prices.length;
-4        int[][][]dp =new int[n+1][2][3];
-5        //cap=0
-6        for(int i=0;i<n;i++){
-7            for(int buy=0;buy<=1;buy++){
-8                dp[i][buy][0]=0;
-9            }
-10        }
-11        // index=n
-12            for(int buy=0;buy<=1;buy++){
-13                for(int cap=0;cap<=2;cap++){
-14                dp[n][buy][cap]=0;
-15            }
-16        }
-17        for(int i=n-1;i>=0;i--){
-18            for(int buy=1;buy>=0;buy--){
-19                for(int cap=2;cap>=1;cap--){
-20            if(buy==1){
-21            dp[i][buy][cap]=Math.max((-prices[i]+dp[i+1][0][cap]), //buy
-22                        (0+dp[i+1][1][cap]));          //not buy       
-23            }else{
-24             dp[i][buy][cap]=Math.max((prices[i]+dp[i+1][1][cap-1]), //sell
-25                        (0+dp[i+1][0][cap]));          //not sell      
-26     }
-27                }
-28            }
-29        }
-30        
-31        return dp[0][1][2];
-32    }
-33}
+3        int n = prices.length;
+4
+5        int[][] ahead = new int[2][3];
+6        int[][] curr = new int[2][3];
+7
+8        for(int i = n - 1; i >= 0; i--) {
+9            for(int buy = 0; buy <= 1; buy++) {
+10                for(int cap = 1; cap <= 2; cap++) {
+11
+12                    if(buy == 1) {
+13                        curr[buy][cap] = Math.max(
+14                            -prices[i] + ahead[0][cap],   // buy
+15                            ahead[1][cap]                 // not buy
+16                        );
+17                    } else {
+18                        curr[buy][cap] = Math.max(
+19                            prices[i] + ahead[1][cap-1],  // sell
+20                            ahead[0][cap]                 // not sell
+21                        );
+22                    }
+23                }
+24            }
+25
+26            // move current row to ahead
+27            ahead=curr;
+28        }
+29
+30        return ahead[1][2];
+31    }
+32}
