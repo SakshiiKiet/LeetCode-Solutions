2d1d347db812e4dbc76797e61f00aada481653d7
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-03 13:13:11 +0530

    [188] [Time Beats: 42.21%] [Memory Beats: 54.62%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock-IV.java
+++ Best-Time-to-Buy-and-Sell-Stock-IV.java
@@ -1,45 +1,22 @@
 1class Solution {
-2    public int maxProfit(int k,int[] prices) {
-3
+2    int[][]dp;
+3    public int maxProfit(int k,int[] prices) {
 4        int n = prices.length;
-5        int[][] ahead = new int[2][k+1];
-6
-7        for(int buy = 0; buy <= 1; buy++){
-8            for(int cap = 0; cap <= k; cap++){
-9                ahead[buy][cap] = 0;
-10            }
-11        }
-12
-13        for(int i = n - 1; i >= 0; i--) {
-14
-15            int[][] curr = new int[2][k+1];
-16
-17            // Explicit base case for cap == 0
-18            for(int buy = 0; buy <= 1; buy++){
-19                curr[buy][0] = 0;
-20            }
+5        dp=new int[n][2*k];
+6        for(int[] rows:dp){
+7            Arrays.fill(rows,-1);
+8        }
+9        return profit(0,0,k,prices);
+10    }
+11    int profit(int i,int trans,int k,int[] prices){
+12        if(i==prices.length || trans==2*k)  return 0;
+13        if(dp[i][trans]!=-1) return dp[i][trans];
+14        if(trans%2==0){ //buy
+15          dp[i][trans]=Math.max((-prices[i]+profit(i+1,trans+1,k,prices)),(0+profit(i+1,trans,k,prices)));
+16        }else{
+17           dp[i][trans]=Math.max((prices[i]+profit(i+1,trans+1,k,prices)),(0+profit(i+1,trans,k,prices))) ;
+18        }
+19        return dp[i][trans];
+20    }
 21
-22            for(int buy = 0; buy <= 1; buy++) {
-23                for(int cap = 1; cap <= k; cap++) {
-24
-25                    if(buy == 1) {
-26                        curr[buy][cap] = Math.max(
-27                            -prices[i] + ahead[0][cap],   // buy
-28                            ahead[1][cap]                 // not buy
-29                        );
-30                    } else {
-31                        curr[buy][cap] = Math.max(
-32                            prices[i] + ahead[1][cap-1],  // sell
-33                            ahead[0][cap]                 // not sell
-34                        );
-35                    }
-36                }
-37            }
-38
-39            // Move current to ahead
-40            ahead = curr;
-41        }
-42
-43        return ahead[1][k];
-44    }
-45}
+22}
