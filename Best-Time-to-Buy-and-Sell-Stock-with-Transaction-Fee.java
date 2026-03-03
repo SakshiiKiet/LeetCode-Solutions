23bc2d3e30c331a912a59d80767c2728beb90bf7
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-03 14:33:59 +0530

    [714] [Time Beats: 99.46%] [Memory Beats: 77.86%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.java
+++ Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.java
@@ -1,22 +1,30 @@
 1class Solution {
 2    public int maxProfit(int[] prices,int fee) {
-3        int n=prices.length;
-4      int[][]  dp=new int[n+1][2];
-5       
-6        dp[n][0]=dp[n][1]=0;
-7        for(int i=n-1;i>=0;i--){
-8            for(int buy=1;buy>=0;buy--){
-9                 int profit=0;
-10        if(buy==1){
-11        profit=Math.max((-prices[i]+dp[i+1][0]), //buy
-12                        (0+dp[i+1][1]));          //not buy       
-13     }else{
-14        profit=Math.max((prices[i]-fee+dp[i+1][1]), //sell
-15                        (0+dp[i+1][0]));          //not sell      
-16              }
-17              dp[i][buy]=profit;
-18            }
-19        }
-20        return dp[0][1];
-21    }
-22}
+3        int n = prices.length;
+4
+5        int[] ahead = new int[2];  // dp[i+1]
+6        int[] curr = new int[2];   // dp[i]
+7
+8        ahead[0] = ahead[1] = 0;
+9
+10        for(int i = n - 1; i >= 0; i--){
+11            for(int buy = 0; buy <= 1; buy++){
+12
+13                if(buy == 1){
+14                    curr[buy] = Math.max(
+15                        -prices[i] + ahead[0],  // buy
+16                        ahead[1]                // not buy
+17                    );
+18                } else {
+19                    curr[buy] = Math.max(
+20                        prices[i]-fee + ahead[1],   // sell
+21                        ahead[0]                // not sell
+22                    );
+23                }
+24            }
+25            ahead = curr;  // move to next iteration
+26        }
+27
+28        return ahead[1];  // start with buy allowed
+29    }
+30}
