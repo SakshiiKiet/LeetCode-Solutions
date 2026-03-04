0f05fced99747710b8c1bea7b406e56fc66134c5
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-04 10:56:24 +0530

    [300] [Time Beats: 64.94%] [Memory Beats: 69.52%] - LeetPush

--- Longest-Increasing-Subsequence.java
+++ Longest-Increasing-Subsequence.java
@@ -1,22 +1,17 @@
 1class Solution {
 2    public int lengthOfLIS(int[] nums) {
 3        int n=nums.length;
-4        int[] ahead=new int[n+1];
-5        for(int prev=0;prev<=n;prev++){
-6            ahead[prev]=0;
-7        }
-8        for(int i=n-1;i>=0;i--){
-9            int[] curr=new int[n+1];
-10            for(int prev=i-1;prev>=-1;prev--){
-11              int notTake=0+ahead[prev+1]; 
-12        int take=0;
-13        if(prev==-1 || nums[i]>nums[prev]){
-14             take=1+ahead[i+1]; 
-15        }
-16         curr[prev+1]=Math.max(take,notTake);
-17            }
-18            ahead=curr;
-19        }
-20        return ahead[0];
-21    }
-22}
+4        int[]dp=new int[n];
+5        int max=1;
+6        Arrays.fill(dp,1);
+7        for(int i=0;i<n;i++){
+8            for(int prev=0;prev<i;prev++){
+9                if(nums[prev]<nums[i]){
+10                    dp[i]=Math.max(dp[i],1+dp[prev]);
+11                }
+12            }
+13            max=Math.max(max,dp[i]);
+14        }
+15        return max;
+16    }
+17}
