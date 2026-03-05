1a14339fb878da07be6a21b24ccf9a988d1962f9
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-05 07:53:44 +0530

    [300] [Time Beats: 81.93%] [Memory Beats: 41.80%] - LeetPush

--- Longest-Increasing-Subsequence.java
+++ Longest-Increasing-Subsequence.java
@@ -1,17 +1,22 @@
 1class Solution {
 2    public int lengthOfLIS(int[] nums) {
 3        int n=nums.length;
-4        int[]dp=new int[n];
-5        int max=1;
-6        Arrays.fill(dp,1);
-7        for(int i=0;i<n;i++){
-8            for(int prev=0;prev<i;prev++){
-9                if(nums[prev]<nums[i]){
-10                    dp[i]=Math.max(dp[i],1+dp[prev]);
-11                }
-12            }
-13            max=Math.max(max,dp[i]);
-14        }
-15        return max;
-16    }
-17}
+4        ArrayList<Integer>temp=new ArrayList<>();
+5        temp.add(nums[0]);
+6        int len=1;
+7        for(int i=1;i<n;i++){
+8            if(nums[i]>temp.get(temp.size()-1)){
+9                temp.add(nums[i]);
+10                len++;
+11            }else{
+12               int ind = Collections.binarySearch(temp, nums[i]);
+13
+14                if(ind < 0)
+15                    ind = -(ind + 1);   // insertion point
+16
+17                temp.set(ind, nums[i]);
+18            }
+19        }
+20        return len;
+21    }
+22}
