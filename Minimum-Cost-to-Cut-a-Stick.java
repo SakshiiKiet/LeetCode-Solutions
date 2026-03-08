8fc8e820c4ff7acc3de3d229568112f8be27de2d
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-08 21:18:44 +0530

    [1547] [Time Beats: 67.33%] [Memory Beats: 75.02%] - LeetPush

--- Minimum-Cost-to-Cut-a-Stick.java
+++ Minimum-Cost-to-Cut-a-Stick.java
@@ -1,34 +1,40 @@
 1class Solution {
-2    int[][]dp;
+2
 3    public int minCost(int n, int[] cuts) {
-4        int c=cuts.length; //length of array
-5        dp=new int[c+1][c+1];
-6        for (int[] row : dp) {
-7            Arrays.fill(row, -1);
-8        }
-9        List<Integer>cut=new ArrayList<>();
-10        for(int x:cuts){
-11            cut.add(x);
-12        }
-13        cut.add(n);
-14        cut.add(0,0);
-15        Collections.sort(cut);
-16        return f(1,c,cut);
-17    }
-18    int f(int i,int j,List<Integer> cuts){
-19         if (i > j) return 0;
-20         if(dp[i][j]!=-1) return dp[i][j];
-21        int min = Integer.MAX_VALUE;
+4
+5        int c = cuts.length;
+6
+7        List<Integer> cut = new ArrayList<>();
+8        for (int x : cuts) {
+9            cut.add(x);
+10        }
+11
+12        cut.add(n);
+13        cut.add(0, 0);
+14        Collections.sort(cut);
+15
+16        int[][] dp = new int[c + 2][c + 2];
+17
+18        for (int i = c; i >= 1; i--) {
+19            for (int j = 1; j <= c; j++) {
+20
+21                if (i > j) continue;
 22
-23        for (int ind = i; ind <= j; ind++) {
+23                int min = Integer.MAX_VALUE;
 24
-25            int cost = cuts.get(j + 1) - cuts.get(i - 1)
-26                    + f(i, ind - 1, cuts)
-27                    + f(ind + 1, j, cuts);
-28
-29            min = Math.min(min, cost);
-30        }
-31
-32        return dp[i][j]=min;
-33    }
-34}
+25                for (int ind = i; ind <= j; ind++) {
+26
+27                    int cost = cut.get(j + 1) - cut.get(i - 1)
+28                            + dp[i][ind - 1]
+29                            + dp[ind + 1][j];
+30
+31                    min = Math.min(min, cost);
+32                }
+33
+34                dp[i][j] = min;
+35            }
+36        }
+37
+38        return dp[1][c];
+39    }
+40}
