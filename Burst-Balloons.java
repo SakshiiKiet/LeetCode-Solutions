1dd278320d59bf547dee679c431d8bcade60ca03
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-16 19:00:19 +0530

    [312] [Time Beats: 94.89%] [Memory Beats: 89.46%] - LeetPush

--- Burst-Balloons.java
+++ Burst-Balloons.java
@@ -1,30 +1,39 @@
 1class Solution {
-2    int[][]dp;
-3    public int maxCoins(int[] nums) {
-4        int n=nums.length;
-5        dp=new int[n+1][n+1];
-6        for (int[] row : dp) {
-7            Arrays.fill(row, -1);
-8        }
-9        List<Integer>num=new ArrayList<>();
-10        for (int x : nums) {
-11            num.add(x);
-12        }
-13
-14        num.add(1);
-15        num.add(0, 1);
-16        return maxC(1,nums.length,num);
-17    }
-18    int maxC(int i,int j,List<Integer> num){
-19        if(i>j) return 0;
-20        if(dp[i][j]!=-1) return dp[i][j];
-21        int max=Integer.MIN_VALUE;
-22        for(int ind=i;ind<=j;ind++){
-23            int cost= num.get(i - 1) * num.get(ind) * num.get(j + 1)
-24                    + maxC(i, ind - 1, num)
-25                    + maxC(ind + 1, j, num);
-26            max=Math.max(max,cost);
-27        }
-28        return dp[i][j]=max;
-29    }
-30}
+2    public int maxCoins(int[] nums) {
+3
+4        int n = nums.length;
+5        int[] arr = new int[n + 2];
+6        arr[0] = 1;
+7        arr[n + 1] = 1;
+8
+9        for (int i = 0; i < n; i++) {
+10            arr[i + 1] = nums[i];
+11        }
+12
+13        int[][] dp = new int[n + 2][n + 2];
+14
+15        // fill dp bottom up
+16        for (int i = n; i >= 1; i--) {
+17            for (int j = 1; j <= n; j++) {
+18
+19                if (i > j) continue;
+20
+21                int max = 0;
+22
+23                for (int k = i; k <= j; k++) {
+24
+25                    int cost =
+26                            arr[i - 1] * arr[k] * arr[j + 1]
+27                            + dp[i][k - 1]
+28                            + dp[k + 1][j];
+29
+30                    max = Math.max(max, cost);
+31                }
+32
+33                dp[i][j] = max;
+34            }
+35        }
+36
+37        return dp[1][n];
+38    }
+39}
