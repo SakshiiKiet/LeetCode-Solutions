0437d32f77812570c31a0b420e220b1404d253f0
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-17 13:57:27 +0530

    [1043] [Time Beats: 54.16%] [Memory Beats: 57.55%] - LeetPush

--- Partition-Array-for-Maximum-Sum.java
+++ Partition-Array-for-Maximum-Sum.java
@@ -1,23 +1,20 @@
 1class Solution {
-2    int[] dp;
-3    public int maxSumAfterPartitioning(int[] arr, int k) {
-4        int n=arr.length;
-5        dp=new int[n];
-6        Arrays.fill(dp,-1);
-7        return func(0,k,arr);
-8    }
-9    int func(int index,int k,int arr[]){
-10        if(index==arr.length) return 0;
-11        if(dp[index]!=-1) return dp[index];
-12        int maxAns=Integer.MIN_VALUE;
-13        int len=0;
-14        int max=Integer.MIN_VALUE;
-15        for(int i=index;i<Math.min(arr.length,index+k);i++){
-16            len++;
-17            max=Math.max(max,arr[i]);
-18            int sum=(len*max)+func(i+1,k,arr);
-19            maxAns=Math.max(maxAns,sum);
-20        }
-21        return dp[index]=maxAns;
-22    }
-23}
+2    public int maxSumAfterPartitioning(int[] arr, int k) {
+3        int n=arr.length;
+4         int[] dp=new int[n+1];
+5         dp[n]=0;
+6         for(int index=n-1;index>=0;index--){
+7          int maxAns=Integer.MIN_VALUE;
+8        int len=0;
+9        int max=Integer.MIN_VALUE;
+10        for(int i=index;i<Math.min(arr.length,index+k);i++){
+11            len++;
+12            max=Math.max(max,arr[i]);
+13            int sum=(len*max)+dp[i+1];
+14            maxAns=Math.max(maxAns,sum);
+15        }
+16        dp[index]=maxAns;
+17         }
+18         return dp[0];
+19    }
+20}
