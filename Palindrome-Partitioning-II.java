e5359692d456d02c41ba730caad63554171c835c
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-17 11:42:39 +0530

    [132] [Time Beats: 15.53%] [Memory Beats: 76.77%] - LeetPush

--- Palindrome-Partitioning-II.java
+++ Palindrome-Partitioning-II.java
@@ -1,30 +1,24 @@
 1class Solution {
-2    int[] dp;
-3    int minCut(String s) {
-4        int n=s.length();
-5        dp=new int[n];
-6        Arrays.fill(dp,-1);
-7        return solve(0, s) - 1; // cuts = partitions - 1
-8    }
-9
-10    int solve(int index, String s) {
-11        if (index == s.length()) return 0;
-12        if(dp[index]!=-1) return dp[index];
-13        int min = Integer.MAX_VALUE;
-14
-15        for (int i = index; i < s.length(); i++) {
-16            if (isPalindrome(s, index, i)) {
-17                min = Math.min(min, 1 + solve(i + 1, s));
-18            }
-19        }
-20        return dp[index]=min;
-21    }
-22
-23    boolean isPalindrome(String s, int l, int r) {
-24        while (l < r) {
-25            if (s.charAt(l++) != s.charAt(r--)) return false;
-26        }
-27        return true;
-28    }
-29}
-30
+2    boolean isPalindrome(String s, int l, int r) {
+3        while (l < r) {
+4            if (s.charAt(l++) != s.charAt(r--)) return false;
+5        }
+6        return true;
+7    }
+8    int minCut(String s) {
+9        int n=s.length();
+10        int[] dp=new int[n+1];
+11        dp[n]=0;
+12       for(int index=n-1;index>=0;index--){
+13       int min = Integer.MAX_VALUE;
+14        for (int i = index; i < s.length(); i++) {
+15            if (isPalindrome(s, index, i)) {
+16                min = Math.min(min, 1 + dp[i + 1]);
+17            }
+18        }
+19        dp[index]=min;
+20       }
+21        return dp[0]-1;
+22    }
+23}
+24
