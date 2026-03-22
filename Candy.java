07393cebc3bd6715bf4de8391732717c70764b94
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-22 21:39:54 +0530

    [135] [Time Beats: 86.33%] [Memory Beats: 12.74%] - LeetPush

--- Candy.java
+++ Candy.java
@@ -1,26 +1,40 @@
 1class Solution {
 2    public int candy(int[] ratings) {
-3        int n=ratings.length;
-4        int[] lc=new int[n];
-5        Arrays.fill(lc,1);
-6        for(int i=1;i<n;i++){
-7            if(ratings[i]>ratings[i-1]){
-8            lc[i]=lc[i-1]+1;
-9        }
-10        }
-11          int sum = Math.max(lc[n - 1], 1);
-12        int right = 1;
-13
-14        for (int i = n - 2; i >= 0; i--) {
-15            if (ratings[i] > ratings[i + 1]) {
-16                right++;
-17            } else {
-18                right = 1;
-19            }
-20
-21            sum += Math.max(lc[i], right);
-22        }
+3        int n = ratings.length;
+4        int sum = 1;     // first child gets 1 candy
+5        int i = 1;
+6
+7        while (i < n) {
+8
+9            // CASE 1: equal ratings
+10            if (ratings[i] == ratings[i - 1]) {
+11                sum += 1;
+12                i++;
+13                continue;
+14            }
+15
+16            // CASE 2: increasing slope
+17            int peak = 1;
+18            while (i < n && ratings[i] > ratings[i - 1]) {
+19                peak++;
+20                sum += peak;
+21                i++;
+22            }
 23
-24        return sum;
-25    }
-26}
+24            // CASE 3: decreasing slope
+25            int down = 1;
+26            while (i < n && ratings[i] < ratings[i - 1]) {
+27                sum += down;
+28                down++;
+29                i++;
+30            }
+31
+32            // peak correction
+33            if (down > peak) {
+34                sum += (down - peak);
+35            }
+36        }
+37
+38        return sum;
+39    }
+40}
