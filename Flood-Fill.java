ea2fa3f39207ea34f72915549c3ada09a46afbe7
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-24 10:34:29 +0530

    [733] [Time Beats: 44.29%] [Memory Beats: 20.79%] - LeetPush

--- Flood-Fill.java
+++ Flood-Fill.java
@@ -5,31 +5,41 @@
 5        this.col = col;
 6    }
 7}
-8class Solution {
-9    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-10        int m=image.length;
-11        int n=image[0].length;
-12        int initial=image[sr][sc];
-13        if(initial==color) return image;
-14        int[][] vis=image;
-15        Queue<Pair>q=new LinkedList<>();
-16        q.offer(new Pair(sr,sc));
-17        vis[sr][sc]=color;
-18        int drow[]={-1,0,+1,0};
-19        int dcol[]={0,1,0,-1};
-20        while(!q.isEmpty()){
-21            int r=q.peek().row;
-22            int c=q.peek().col;
-23            q.poll();
-24         for(int i=0;i<4;i++){
-25            int nrow=r+drow[i];
-26            int ncol=c+dcol[i];
-27            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && image[nrow][ncol]==initial && vis[nrow][ncol]!=color){
-28                q.offer(new Pair(nrow,ncol));
-29                vis[nrow][ncol]=color;
-30            }
-31         }
-32        }
-33     return vis;
-34    }
-35}
+8
+9class Solution {
+10    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+11
+12        int m = image.length;
+13        int n = image[0].length;
+14
+15        int initial = image[sr][sc];
+16        if(initial == color) return image;
+17
+18        Queue<Pair> q = new LinkedList<>();
+19        q.offer(new Pair(sr, sc));
+20
+21        image[sr][sc] = color;
+22
+23        int[] drow = {-1,0,1,0};
+24        int[] dcol = {0,1,0,-1};
+25
+26        while(!q.isEmpty()){
+27            Pair p = q.poll();
+28            int r = p.row;
+29            int c = p.col;
+30
+31            for(int i = 0; i < 4; i++){
+32                int nrow = r + drow[i];
+33                int ncol = c + dcol[i];
+34
+35                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n
+36                   && image[nrow][ncol] == initial){
+37
+38                    image[nrow][ncol] = color;
+39                    q.offer(new Pair(nrow, ncol));
+40                }
+41            }
+42        }
+43        return image;
+44    }
+45}
