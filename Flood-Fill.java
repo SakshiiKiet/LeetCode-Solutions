0a9362cc1b7cc1402dc12f989c7d1790f7975bd0
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-24 11:19:40 +0530

    [733] [Time Beats: 100.00%] [Memory Beats: 57.27%] - LeetPush

--- Flood-Fill.java
+++ Flood-Fill.java
@@ -1,45 +1,25 @@
-1class Pair {
-2    int row, col;
-3    Pair(int row, int col){
-4        this.row = row;
-5        this.col = col;
-6    }
-7}
-8
-9class Solution {
-10    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-11
-12        int m = image.length;
-13        int n = image[0].length;
-14
-15        int initial = image[sr][sc];
-16        if(initial == color) return image;
-17
-18        Queue<Pair> q = new LinkedList<>();
-19        q.offer(new Pair(sr, sc));
+1class Solution {
+2    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+3        int initial=image[sr][sc];
+4         if(initial==color) return image;
+5        int[][] vis=image;
+6        int[] drow={-1,0,1,0};
+7        int[] dcol={0,1,0,-1};
+8        dfs(sr,sc,color,initial,image,vis,drow,dcol);
+9        return vis;
+10    }
+11    void dfs(int row,int col,int newcolor,int initialcolor,int[][] image,int[][] vis,int[] drow,int[] dcol){
+12       vis[row][col]=newcolor;
+13       int m=image.length;
+14       int n=image[0].length;
+15       for(int i=0;i<4;i++){
+16         int nrow = row + drow[i];
+17            int ncol = col + dcol[i];
+18
+19        if(nrow >= 0 && nrow < m &&ncol >= 0 && ncol < n &&image[nrow][ncol] == initialcolor && vis[nrow][ncol] != newcolor) {
 20
-21        image[sr][sc] = color;
-22
-23        int[] drow = {-1,0,1,0};
-24        int[] dcol = {0,1,0,-1};
-25
-26        while(!q.isEmpty()){
-27            Pair p = q.poll();
-28            int r = p.row;
-29            int c = p.col;
-30
-31            for(int i = 0; i < 4; i++){
-32                int nrow = r + drow[i];
-33                int ncol = c + dcol[i];
-34
-35                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n
-36                   && image[nrow][ncol] == initial){
-37
-38                    image[nrow][ncol] = color;
-39                    q.offer(new Pair(nrow, ncol));
-40                }
-41            }
-42        }
-43        return image;
-44    }
-45}
+21                dfs(nrow, ncol,newcolor,initialcolor,image,vis,drow,dcol);
+22            }
+23       }
+24    }
+25}
