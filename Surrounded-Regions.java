88fa0eca86dd2ebbbae3b4ddc1fd96b18f0c92ab
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-25 15:03:51 +0530

    [130] [Time Beats: 17.95%] [Memory Beats: 82.00%] - LeetPush

--- Surrounded-Regions.java
+++ Surrounded-Regions.java
@@ -1,50 +1,69 @@
 1class Solution {
-2    public void solve(char[][] board) {
-3        int m=board.length;
-4        int n=board[0].length;
-5        int[][] vis=new int[m][n];
-6        int[] drow={-1,0,+1,0};
-7        int[] dcol={0,+1,0,-1};
-8        for(int j=0;j<n;j++){
-9            //first row
-10            if(vis[0][j]==0 && board[0][j]=='O'){
-11                dfs(0,j,board,vis,drow,dcol);
-12            }
-13
-14            //last row
-15            if(vis[m-1][j]==0 && board[m-1][j]=='O'){
-16                dfs(m-1,j,board,vis,drow,dcol);
-17            }
-18        }
-19        for(int i=0;i<m;i++){
-20            //first col
-21            if(vis[i][0]==0 && board[i][0]=='O'){
-22                dfs(i,0,board,vis,drow,dcol);
-23            }
-24
-25            //last col
-26            if(vis[i][n-1]==0 && board[i][n-1]=='O'){
-27                dfs(i,n-1,board,vis,drow,dcol);
-28            }
-29        }
-30        for(int i=0;i<m;i++){
-31            for(int j=0;j<n;j++){
-32                if(vis[i][j]==0 && board[i][j]=='O'){
-33                    board[i][j]='X';
-34                }
-35            }
-36        }
-37    }
-38    void dfs(int row,int col,char[][] board,int[][] vis,int[] drow,int[] dcol){
-39         int m=board.length;
-40        int n=board[0].length;
-41        vis[row][col]=1;
-42        for(int i=0;i<4;i++){
-43            int nrow=row+drow[i];
-44            int ncol=col+dcol[i];
-45            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && board[nrow][ncol]=='O'){
-46             dfs(nrow,ncol,board,vis,drow,dcol);
-47            }
-48        }
-49    }
-50}
+2
+3    static class Pair {
+4        int row;
+5        int col;
+6        Pair(int r, int c) {
+7            row = r;
+8            col = c;
+9        }
+10    }
+11
+12    public void solve(char[][] board) {
+13        int m = board.length;
+14        int n = board[0].length;
+15
+16        int[][] vis = new int[m][n];
+17        int[] drow = {-1, 0, 1, 0};
+18        int[] dcol = {0, 1, 0, -1};
+19
+20        Queue<Pair> q = new LinkedList<>();
+21        for(int j = 0; j < n; j++) {
+22            if (board[0][j] == 'O') {
+23                q.offer(new Pair(0, j));
+24                vis[0][j] = 1;
+25            }
+26            if (board[m - 1][j] == 'O') {
+27                q.offer(new Pair(m - 1, j));
+28                vis[m - 1][j] = 1;
+29            }
+30        }
+31
+32        for (int i = 0; i < m; i++) {
+33            if (board[i][0] == 'O') {
+34                q.offer(new Pair(i, 0));
+35                vis[i][0] = 1;
+36            }
+37            if (board[i][n - 1] == 'O') {
+38                q.offer(new Pair(i, n - 1));
+39                vis[i][n - 1] = 1;
+40            }
+41        }
+42        while (!q.isEmpty()) {
+43            Pair cur = q.poll();
+44
+45            for (int i = 0; i < 4; i++) {
+46                int nrow = cur.row + drow[i];
+47                int ncol = cur.col + dcol[i];
+48
+49                if (nrow >= 0 && nrow < m &&
+50                    ncol >= 0 && ncol < n &&
+51                    vis[nrow][ncol] == 0 &&
+52                    board[nrow][ncol] == 'O') {
+53
+54                    vis[nrow][ncol] = 1;
+55                    q.offer(new Pair(nrow, ncol));
+56                }
+57            }
+58        }
+59
+60        for (int i = 0; i < m; i++) {
+61            for (int j = 0; j < n; j++) {
+62                if (vis[i][j] == 0 && board[i][j] == 'O') {
+63                    board[i][j] = 'X';
+64                }
+65            }
+66        }
+67    }
+68    }
+69
