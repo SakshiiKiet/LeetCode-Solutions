57d095e93c9ab285de752c41b4317dcb0bcc60f9
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-26 13:51:29 +0530

    [200] [Time Beats: 10.77%] [Memory Beats: 96.68%] - LeetPush

--- Number-of-Islands.java
+++ Number-of-Islands.java
@@ -1,31 +1,43 @@
-1class Solution {
-2    public int numIslands(char[][] grid) {
-3         int m = grid.length;
-4        int n = grid[0].length;
-5        int[][] vis=new int[m][n];
-6        int[] drow={-1,0,+1,0};
-7        int[] dcol={0,+1,0,-1};
-8        int c=0;
-9        for(int i=0;i<m;i++){
-10            for(int j=0;j<n;j++){
-11                if(vis[i][j]==0 && grid[i][j]=='1'){
-12                    c++;
-13                    dfs(i,j,grid,vis,drow,dcol);
-14                }
-15            }
-16        }
-17        return c;
-18    }
-19    void dfs(int row,int col,char[][] grid,int[][] vis,int[] drow,int[] dcol){
-20        int m = grid.length;
-21        int n = grid[0].length;
-22        vis[row][col]=1;
-23        for(int i=0;i<4;i++){
-24            int nrow=row+drow[i];
-25            int ncol=col+dcol[i];
-26            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]=='1'){
-27             dfs(nrow,ncol,grid,vis,drow,dcol);
-28            }
-29        }
-30    }
-31}
+1class Pair {
+2        int row;
+3        int col;
+4        Pair(int r, int c) {
+5            row = r;
+6            col = c;
+7        }
+8    }
+9class Solution {
+10    public int numIslands(char[][] grid) {
+11         int m = grid.length;
+12        int n = grid[0].length;
+13        int[][] vis=new int[m][n];
+14        int[] drow={-1,0,+1,0};
+15        int[] dcol={0,+1,0,-1};
+16        Queue<Pair>q=new LinkedList<>();
+17        int c=0;
+18        for(int i=0;i<m;i++){
+19         for(int j=0;j<n;j++){
+20          if(vis[i][j]==0 && grid[i][j]=='1'){
+21            c++;
+22            q.offer(new Pair(i,j));
+23            vis[i][j]=1;
+24            while(!q.isEmpty()){
+25              int row=q.peek().row;
+26              int col=q.peek().col;
+27              q.poll();
+28                for(int k=0;k<4;k++){
+29                int nrow=row+drow[k];
+30                int ncol=col+dcol[k];
+31                if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&vis[nrow][ncol] == 0 && grid[nrow][ncol] == '1') {
+32
+33                    vis[nrow][ncol] = 1;
+34                    q.offer(new Pair(nrow, ncol));
+35                  }
+36                }
+37              }
+38            }
+39          }     
+40        }
+41        return c;
+42    }
+43}
