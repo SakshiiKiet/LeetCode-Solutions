19ca3543dec0c38e0f8205b45aff52af8b7ebff5
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-26 12:39:00 +0530

    [1020] [Time Beats: 30.26%] [Memory Beats: 76.15%] - LeetPush

--- Number-of-Enclaves.java
+++ Number-of-Enclaves.java
@@ -1,52 +1,52 @@
-1class Solution {
-2    public int numEnclaves(int[][] grid) {
-3         int m=grid.length;
-4        int n=grid[0].length;
-5        int[][] vis=new int[m][n];
-6        int[] drow={-1,0,+1,0};
-7        int[] dcol={0,+1,0,-1};
-8        for(int j=0;j<n;j++){
-9            //first row
-10            if(vis[0][j]==0 && grid[0][j]==1){
-11                dfs(0,j,grid,vis,drow,dcol);
-12            }
-13
-14            //last row
-15            if(vis[m-1][j]==0 && grid[m-1][j]==1){
-16                dfs(m-1,j,grid,vis,drow,dcol);
-17            }
-18        }
-19        for(int i=0;i<m;i++){
-20            //first col
-21            if(vis[i][0]==0 && grid[i][0]==1){
-22                dfs(i,0,grid,vis,drow,dcol);
-23            }
-24
-25            //last col
-26            if(vis[i][n-1]==0 && grid[i][n-1]==1){
-27                dfs(i,n-1,grid,vis,drow,dcol);
-28            }
-29        }
-30        int c=0;
-31        for(int i=0;i<m;i++){
-32            for(int j=0;j<n;j++){
-33                if(vis[i][j]==0 && grid[i][j]==1){
-34                    c++;
-35                }
-36            }
-37        }
-38        return c;
-39    }
-40    void dfs(int row,int col,int[][] grid,int[][] vis,int[] drow,int[] dcol){
-41         int m=grid.length;
-42        int n=grid[0].length;
-43        vis[row][col]=1;
-44        for(int i=0;i<4;i++){
-45            int nrow=row+drow[i];
-46            int ncol=col+dcol[i];
-47            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-48             dfs(nrow,ncol,grid,vis,drow,dcol);
-49            }
-50        }
-51    }
+1class Pair {
+2        int row;
+3        int col;
+4        Pair(int r, int c) {
+5            row = r;
+6            col = c;
+7        }
+8    }
+9class Solution {
+10    public int numEnclaves(int[][] grid) {
+11         int m=grid.length;
+12        int n=grid[0].length;
+13        int[][] vis=new int[m][n];
+14        int[] drow={-1,0,+1,0};
+15        int[] dcol={0,+1,0,-1};
+16        Queue<Pair>q=new LinkedList<>();
+17        for(int i=0;i<m;i++){
+18            for(int j=0;j<n;j++){
+19                if(i==0 || j==0 || i==m-1 || j==n-1){
+20                    if(grid[i][j]==1){
+21                        q.offer(new Pair(i,j));
+22                        vis[i][j]=1;
+23                    }
+24                }
+25            }
+26        }
+27        while(!q.isEmpty()){
+28            int row=q.peek().row;
+29            int col=q.peek().col;
+30            q.poll();
+31            for(int i=0;i<4;i++){
+32             int nrow=row+drow[i];
+33             int ncol=col+dcol[i];
+34             if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
+35
+36                    vis[nrow][ncol] = 1;
+37                    q.offer(new Pair(nrow, ncol));
+38                }
+39            }
+40        }
+41        int c=0;
+42        for(int i=0;i<m;i++){
+43            for(int j=0;j<n;j++){
+44                if(vis[i][j]==0 && grid[i][j]==1){
+45                    c++;
+46                }
+47            }
+48        }
+49        return c;
+50    }
+51    
 52}
