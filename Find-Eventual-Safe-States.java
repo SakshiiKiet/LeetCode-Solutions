53ca0e10182418afc99437bf001a7616d5504afa
Sakshi Maan <sakshimaan2004@gmail.com>
2026-03-30 12:19:52 +0530

    [802] [Time Beats: 71.86%] [Memory Beats: 11.57%] - LeetPush

--- Find-Eventual-Safe-States.java
+++ Find-Eventual-Safe-States.java
@@ -1,37 +1,40 @@
 1class Solution {
 2    public List<Integer> eventualSafeNodes(int[][] graph) {
-3         int V=graph.length;
-4        
-5        List<List<Integer>>adjrev=new ArrayList<>();
-6        for(int i=0;i<V;i++){
-7            adjrev.add(new ArrayList<>());
-8        }
-9        int[] indegree=new int[V];
-10        for(int i=0;i<V;i++){
-11            for(int adjNode:graph[i]){
-12                adjrev.get(adjNode).add(i);
-13                indegree[i]++;
-14            }
-15        }
-16        List<Integer>safe=new ArrayList<>();
-17        Queue<Integer>q=new LinkedList<>();
-18        for(int i=0;i<V;i++){
-19            if(indegree[i]==0){
-20                q.offer(i);
-21            }
-22        }
-23         while(!q.isEmpty()){
-24            int node=q.peek();
-25            q.poll();
-26            safe.add(node);
-27            for(int adjNode:adjrev.get(node)){
-28                indegree[adjNode]--;
-29                if(indegree[adjNode]==0){
-30                    q.offer(adjNode);
-31                }
-32            }
-33         }
-34         Collections.sort(safe);
-35         return safe;
-36    }
-37}
+3     int V=graph.length;
+4     int[] vis=new int[V];
+5     int[] pathvis=new int[V];
+6     int[] check=new int[V];
+7     List<Integer>safe=new ArrayList<>();
+8     for(int i=0;i<V;i++){
+9        if(vis[i]==0){
+10            dfs(i,vis,pathvis,graph,check);
+11        }
+12     }   
+13    for(int i=0;i<V;i++){
+14        if(check[i]==1){
+15            safe.add(i);
+16        }
+17    }
+18    return safe;
+19    }
+20 boolean dfs(int node,int[] vis,int[] pathvis,int[][] graph,int[] check){
+21    vis[node]=1;
+22    pathvis[node]=1;
+23    check[node]=0;
+24    //traverse for adjacent nodes
+25    for(int adjNode:graph[node]){
+26        // if not visited
+27        if(vis[adjNode]==0){
+28            if(dfs(adjNode,vis,pathvis,graph,check)==true){
+29                check[adjNode]=0;                                  //cycle
+30                return true;
+31            }
+32        }else if(pathvis[adjNode]==1){
+33            return true;
+34        }  
+35    }
+36    check[node]=1;
+37    pathvis[node]=0;
+38    return false;
+39 }
+40}
