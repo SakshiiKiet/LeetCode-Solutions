cde5089a2fc0ade4a3dffa90b497c22fa94d597c
Sakshi Maan <sakshimaan2004@gmail.com>
2026-04-10 11:20:39 +0530

    [1319] [Time Beats: 62.27%] [Memory Beats: 56.09%] - LeetPush

---
+++ Number-of-Operations-to-Make-Network-Connected.java
@@ -0,0 +1,71 @@
+1class Solution {
+2
+3    static class DisjointSet {
+4        int[] parent, size;
+5
+6        DisjointSet(int n) {
+7            parent = new int[n];
+8            size = new int[n];
+9
+10            for (int i = 0; i < n; i++) {
+11                parent[i] = i;
+12                size[i] = 1;
+13            }
+14        }
+15
+16        int findPar(int node) {
+17            if (node == parent[node])
+18                return node;
+19            return parent[node] = findPar(parent[node]);
+20        }
+21
+22        void unionBySize(int u, int v) {
+23            int pu = findPar(u);
+24            int pv = findPar(v);
+25
+26            if (pu == pv) return;
+27
+28            if (size[pu] < size[pv]) {
+29                parent[pu] = pv;
+30                size[pv] += size[pu];
+31            } else {
+32                parent[pv] = pu;
+33                size[pu] += size[pv];
+34            }
+35        }
+36    }
+37
+38    public int makeConnected(int n, int[][] connections) {
+39
+40        // Step 1: Check if enough edges exist
+41       int cextra=0;
+42        DisjointSet ds = new DisjointSet(n);
+43
+44        // Step 2: Connect components
+45        for (int[] edge : connections) {
+46            int u = edge[0];
+47            int v = edge[1];
+48            if(ds.findPar(u)==ds.findPar(v)){
+49                cextra++;
+50            }else{
+51                ds.unionBySize(u, v);
+52            }
+53            
+54        }
+55
+56        // Step 3: Count components
+57        int components = 0;
+58        for (int i = 0; i < n; i++) {
+59            if (ds.findPar(i) == i) {
+60                components++;
+61            }
+62        }
+63
+64        // Step 4: Need (components - 1) operations
+65        if(cextra>=components-1){
+66            return components-1;
+67        }else{
+68            return -1;
+69        }
+70    }
+71}
