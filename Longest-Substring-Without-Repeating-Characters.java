032dfc044975c1749cec37078557ba2137f233ac
Sakshi Maan <sakshimaan2004@gmail.com>
2026-04-16 10:08:20 +0530

    [3] [Time Beats: 8.53%] [Memory Beats: 11.70%] - LeetPush

--- Longest-Substring-Without-Repeating-Characters.java
+++ Longest-Substring-Without-Repeating-Characters.java
@@ -2,13 +2,13 @@
 2    public int lengthOfLongestSubstring(String s) {
 3        int maxlen=0;
 4        for(int i=0;i<s.length();i++){
-5            String subs="";
+5            int[] hash=new int[256];
 6            for(int j=i;j<s.length();j++){
-7               if(subs.indexOf(s.charAt(j))!=-1){
+7               if(hash[s.charAt(j)]==1){
 8                break;
 9               }
-10               subs+=s.charAt(j);
-11               maxlen=Math.max(maxlen,subs.length());
+10               hash[s.charAt(j)]=1;
+11               maxlen=Math.max(maxlen,j-i+1);
 12            }
 13        }
 14        return maxlen;
