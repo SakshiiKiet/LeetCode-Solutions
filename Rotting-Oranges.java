18df2a41dcf2f6c04fea15010afbaab4213c1992
Sakshi Maan <sakshimaan2004@gmail.com>
2026-04-17 11:19:59 +0530

    [994] [Time Beats: 5.71%] [Memory Beats: 85.55%] - LeetPush

--- Rotting-Oranges.java
+++ Rotting-Oranges.java
@@ -19,34 +19,36 @@
 19            for(int j=0;j<n;j++){
 20                if(grid[i][j]==2){
 21                    q.offer(new Pair(i,j,0));
-22                    vis[i][j]=2; //if rotten
+22                    vis[i][j]=2;
 23                }else{
-24                    vis[i][j]=0; // if not rotten
+24                    vis[i][j]=0;
 25                }
-26                if(grid[i][j]==1) cntFresh++;
-27            }
-28        }
-29        int time=0;
-30        int drow[]={-1,0,+1,0};
-31        int dcol[]={0,1,0,-1};
-32        int cnt=0;
-33        while(!q.isEmpty()){
-34            int r=q.peek().row;
-35            int c=q.peek().col;
-36            int t=q.peek().time;
-37            time=Math.max(time,t);
-38            q.poll();
-39            for(int i=0;i<4;i++){
-40                int nrow=r+drow[i];
-41                int ncol=c+dcol[i];
-42                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
-43                    q.offer(new Pair(nrow,ncol,t+1));
-44                    vis[nrow][ncol]=2;
-45                    cnt++;
-46                }
-47            }
-48        }
-49        if(cnt!=cntFresh) return -1;
-50        return time;
-51    }
-52}
+26                if(grid[i][j]==1){
+27                    cntFresh++;
+28                }
+29            }
+30        }
+31        int cnt=0;
+32        int time=0;
+33        int drow[]={-1,0,+1,0};
+34        int dcol[]={0,+1,0,-1};
+35        while(!q.isEmpty()){
+36            int r=q.peek().row;
+37            int c=q.peek().col;
+38            int t=q.peek().time;
+39             time=Math.max(time,t);
+40            q.poll();
+41            for(int i=0;i<4;i++){
+42                int nrow=r+drow[i];
+43                int ncol=c+dcol[i];
+44                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
+45                    q.offer(new Pair(nrow,ncol,t+1));
+46                    vis[nrow][ncol]=2;
+47                    cnt++;
+48                }
+49            }
+50        }
+51      if(cnt!=cntFresh) return -1;
+52      return time;
+53    }
+54}
