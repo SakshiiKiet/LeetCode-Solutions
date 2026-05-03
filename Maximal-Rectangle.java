eca364ab4a2521cfe76acbda6bc22eb1d7457737
Sakshi Maan <sakshimaan2004@gmail.com>
2026-05-03 10:10:02 +0530

    [85] [Time Beats: 80.52%] [Memory Beats: 77.12%] - LeetPush

--- Maximal-Rectangle.java
+++ Maximal-Rectangle.java
@@ -1,49 +1,41 @@
-1import java.util.*;
-2
-3class Solution {
-4    public int maximalRectangle(char[][] matrix) {
-5        int n = matrix.length;
-6        int m = matrix[0].length;
-7        int maxArea = 0;
-8
-9        int[] height = new int[m];  
-10
-11        for (int i = 0; i < n; i++) {
-12
-13            for (int j = 0; j < m; j++) {
-14                if (matrix[i][j] == '1') 
-15                    height[j] += 1;
-16                else 
-17                    height[j] = 0;
-18            }
-19
-20            int area = largestRectangleArea(height);
-21            maxArea = Math.max(maxArea, area);
-22        }
-23        return maxArea;
-24    }
-25
-26    public int largestRectangleArea(int[] heights) {
-27        Stack<Integer> st = new Stack<>();
-28        int maxarea = 0;
-29
-30        for (int i = 0; i < heights.length; i++) {
-31            while (!st.isEmpty() && heights[st.peek()] > heights[i]) {
-32                int element = st.pop();
-33                int nse = i;
-34                int pse = st.isEmpty() ? -1 : st.peek();
-35                maxarea = Math.max(maxarea, heights[element] * (nse - pse - 1));
-36            }
-37            st.push(i);
+1class Solution {
+2    public int maximalRectangle(char[][] matrix) {
+3       int n= matrix.length;
+4       int m=matrix[0].length;
+5       int maxarea=0;
+6       int[][] pSum=new int[n][m];
+7       for(int j=0;j<m;j++){
+8        int sum=0;
+9        for(int i=0;i<n;i++){
+10         if(matrix[i][j]=='1') sum+=1;
+11         else sum=0;
+12         pSum[i][j]=sum;
+13        }
+14       }
+15       for(int i=0;i<n;i++){
+16        maxarea=Math.max(maxarea,largestRecArea(pSum[i]));
+17       }
+18   return maxarea;
+19    }
+20    public int largestRecArea(int[] heights){
+21        int n=heights.length;
+22        int maxarea=0;
+23        Stack<Integer>s=new Stack<>();
+24        for(int i=0;i<n;i++){
+25         while(!s.isEmpty() && heights[i]<heights[s.peek()]){
+26            int ele=s.pop();
+27            int nse=i;
+28            int pse=s.isEmpty()?-1:s.peek();
+29            maxarea=Math.max(maxarea,heights[ele]*(nse-pse-1));
+30         }
+31         s.push(i);
+32        }
+33        while (!s.isEmpty()) {
+34            int nse = heights.length;
+35            int ele = s.pop();
+36            int pse = s.isEmpty() ? -1 : s.peek();
+37            maxarea = Math.max(maxarea, heights[ele] * (nse - pse - 1));
 38        }
-39
-40        while (!st.isEmpty()) {
-41            int nse = heights.length;
-42            int element = st.pop();
-43            int pse = st.isEmpty() ? -1 : st.peek();
-44            maxarea = Math.max(maxarea, heights[element] * (nse - pse - 1));
-45        }
-46
-47        return maxarea;
-48    }
-49}
+39        return maxarea;
+40    }
+41}
