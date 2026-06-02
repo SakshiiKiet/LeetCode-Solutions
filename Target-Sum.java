325cfde8d6c4cc321a41973dcc4367116d4e1497
Sakshi Maan <sakshimaan2004@gmail.com>
2026-06-02 06:47:40 +0530

    [494] [Time Beats: 84.78%] [Memory Beats: 44.26%] - LeetPush

--- Target-Sum.java
+++ Target-Sum.java
@@ -1,21 +1,36 @@
-1class Solution {
-2    public int findTargetSumWays(int[] nums, int target) {
-3        return count(nums.length - 1, 0, target, nums);
-4    }
-5
-6    int count(int i, int sum, int target, int[] nums) {
-7        if (i == 0) {
-8            int ways = 0;
-9            if (sum + nums[0] == target) ways++;
-10            if (sum - nums[0] == target) ways++;
-11            return ways;
-12        }
-13
-14        int plus = count(i - 1, sum + nums[i], target, nums);
-15        int minus = count(i - 1, sum - nums[i], target, nums);
-16
-17        return plus + minus;
-18    }
-19}
-20
-21
+1
+2class Solution {
+3    int[][]dp;
+4    public int findTargetSumWays(int[] nums, int target) {
+5       return countPartitions(nums,target);
+6    }
+7       public int countPartitions(int[] arr, int diff) {
+8       int n=arr.length;
+9       int totsum=0;
+10       for(int i=0;i<n;i++){
+11           totsum+=arr[i];
+12       }
+13       if(totsum-diff<0 || ((totsum-diff)%2)!=0) return 0;
+14       int target=(totsum-diff)/2;
+15       dp=new int[n][target+1];
+16       for(int[] rows:dp){
+17           Arrays.fill(rows,-1);
+18       }
+19       return count(n-1,target,arr);
+20    }
+21    int count(int i,int target,int[] arr){
+22       if (i == 0) {
+23            if (target == 0 && arr[0] == 0) return 2;
+24            if (target == 0 || target == arr[0]) return 1;
+25            return 0;
+26        }
+27
+28        if(dp[i][target]!=-1) return dp[i][target];
+29        int notTake=count(i-1,target, arr);
+30        int Take=0;
+31        if(target>=arr[i]){
+32            Take=count(i-1,target-arr[i],arr);
+33        }
+34        return dp[i][target]=notTake+Take;
+35    }
+36}
