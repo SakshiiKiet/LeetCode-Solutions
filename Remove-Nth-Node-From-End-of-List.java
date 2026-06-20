9b0b18072f6a861572ee3903d36b5a5613f66b42
Sakshi Maan <sakshimaan2004@gmail.com>
2026-06-20 17:08:38 +0530

    [19] [Time Beats: 100.00%] [Memory Beats: 39.06%] - LeetPush

--- Remove-Nth-Node-From-End-of-List.java
+++ Remove-Nth-Node-From-End-of-List.java
@@ -12,22 +12,31 @@
 12    public ListNode removeNthFromEnd(ListNode head, int n) {
 13        if (head == null) return null;
 14
-15        ListNode fast=head;
-16        for(int i=0;i<n;i++){
-17            fast=fast.next;
-18        }
-19        if(fast==null){
-20                return head.next;
-21            }
-22        ListNode slow=head;
-23        while(fast.next!=null){
-24            slow=slow.next;
-25            fast=fast.next;
-26            }
-27            
-28       slow.next=slow.next.next;
-29
-30        return head;
-31    }
-32}
-33
+15        // Step 1: count total nodes
+16        int c = 0;
+17        ListNode temp = head;
+18        while (temp != null) {
+19            c++;
+20            temp = temp.next;
+21        }
+22
+23        
+24        if (n == c) {
+25            return head.next;
+26        }
+27
+28        int res=c-n;
+29        temp = head;
+30        while(temp!=null){
+31            res--;
+32            if(res==0){
+33                temp.next=temp.next.next;
+34                break;
+35            }
+36            temp=temp.next;
+37        }
+38
+39        return head;
+40    }
+41}
+42
