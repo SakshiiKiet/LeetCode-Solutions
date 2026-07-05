1040e61c151e7e5fe6c8d7a00235217d560bbdf8
Sakshi Maan <sakshimaan2004@gmail.com>
2026-07-05 21:52:30 +0530

    [2104] [Time Beats: 16.21%] [Memory Beats: 31.38%] - LeetPush

---
+++ 2104-Sum-of-Subarray-Ranges.java
@@ -0,0 +1,95 @@
+import java.util.*;
+
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        return sumSubarrayMaxs(nums) - sumSubarrayMins(nums);
+    }
+
+    // --- Next Smaller Element ---
+    private int[] findNSE(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+            ans[i] = !st.isEmpty() ? st.peek() : n;
+            st.push(i);
+        }
+        return ans;
+    }
+
+    // --- Next Greater Element ---
+    private int[] findNGE(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
+                st.pop();
+            }
+            ans[i] = !st.isEmpty() ? st.peek() : n;
+            st.push(i);
+        }
+        return ans;
+    }
+
+    // --- Previous Smaller or Equal Element ---
+    private int[] findPSEE(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                st.pop();
+            }
+            ans[i] = !st.isEmpty() ? st.peek() : -1;
+            st.push(i);
+        }
+        return ans;
+    }
+
+    // --- Previous Greater or Equal Element ---
+    private int[] findPGEE(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] < arr[i]) {
+                st.pop();
+            }
+            ans[i] = !st.isEmpty() ? st.peek() : -1;
+            st.push(i);
+        }
+        return ans;
+    }
+
+    // --- Sum of Subarray Minimums ---
+    private long sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        int[] nse = findNSE(arr);
+        int[] psee = findPSEE(arr);
+        long total = 0;
+        for (int i = 0; i < n; i++) {
+            long left = i - psee[i];
+            long right = nse[i] - i;
+            total += (long) arr[i] * left * right;
+        }
+        return total;
+    }
+
+    // --- Sum of Subarray Maximums ---
+    private long sumSubarrayMaxs(int[] arr) {
+        int n = arr.length;
+        int[] nge = findNGE(arr);
+        int[] pgee = findPGEE(arr);
+        long total = 0;
+        for (int i = 0; i < n; i++) {
+            long left = i - pgee[i];
+            long right = nge[i] - i;
+            total += (long) arr[i] * left * right;
+        }
+        return total;
+    }
+}
