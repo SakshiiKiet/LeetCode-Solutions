188d86abc05e9213c217a05fd095e468690bfb50
Sakshi Maan <sakshimaan2004@gmail.com>
2026-07-12 07:10:51 +0530

    [1004] [Time Beats: 44.78%] [Memory Beats: 54.50%] - LeetPush

---
+++ 1004-Max-Consecutive-Ones-III.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int l=0;
+        int max=0;int zeroes=0;
+        for(int r=0;r<nums.length;r++){
+            if(nums[r]==0){
+                zeroes++;
+            }
+            while(zeroes>k){
+                if(nums[l]==0){
+                    zeroes--;
+                }
+                l++;
+            }
+            if(zeroes<=k){
+                max=Math.max(max,r-l+1);
+            }
+        }
+        return max;
+    }
+}
