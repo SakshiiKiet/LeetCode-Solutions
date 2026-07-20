97cf80619343e5b56c1c46c296e00950d5e9bba9
Sakshi Maan <sakshimaan2004@gmail.com>
2026-07-20 22:13:28 +0530

    [26] [Time Beats: 6.23%] [Memory Beats: 9.33%] - LeetPush

---
+++ 26-Remove-Duplicates-from-Sorted-Array.java
@@ -0,0 +1,20 @@
+import java.util.LinkedHashSet;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int index = 0;
+        for (int num : set) {
+            nums[index] = num;
+            index++;
+        }
+
+        return index;
+    }
+}
