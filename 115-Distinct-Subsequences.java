e1e7aac57a31e0e20c2a98b7a3e1b4d09c70adeb
Sakshi Maan <sakshimaan2004@gmail.com>
2026-08-19 13:32:02 +0530

    [115] [Time Beats: 52.84%] [Memory Beats: 64.15%] - LeetPush

---
+++ 115-Distinct-Subsequences.java
@@ -0,0 +1,21 @@
+class Solution {
+    int[][] dp;
+    public int numDistinct(String s, String t) {
+        dp=new int[s.length()+1][t.length()+1];
+        for(int[] rows:dp){
+            Arrays.fill(rows,-1);
+        }
+        return solve(s.length(),t.length(),s,t);
+    }
+    int solve(int i,int j,String s, String t){
+        if(j==0) return 1;
+        if(i==0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s.charAt(i-1)==t.charAt(j-1)){
+            return dp[i][j]=solve(i-1,j-1,s,t)+solve(i-1,j,s,t);
+        }else{
+            return dp[i][j]=solve(i-1,j,s,t);
+        }
+       
+    }
+}
