59079724a0cf3f88dead3d5ba9ffb66acd2902b7
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-03 06:19:41 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 56.70%] - LeetPush

--- 198-House-Robber.java
+++ 198-House-Robber.java
@@ -1,18 +1,14 @@
 class Solution {
-    int[] dp;
     public int rob(int[] nums) {
-        dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return solve(nums.length-1,nums);
-    }
-    int solve(int i, int[] nums){
-        if(i==0) return nums[0];
-        if(dp[i]!=-1) return dp[i];
-        int nonpick=solve(i-1,nums);
-        int pick=nums[i];
-        if(i>1){
-            pick+=solve(i-2,nums);
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1;i<nums.length;i++){
+            int not=0+prev;
+            int pick=nums[i]+prev2;
+            int curr=Math.max(not,pick);
+            prev2=prev;
+            prev=curr;
         }
-        return dp[i]=Math.max(pick,nonpick);
+        return prev;
     }
 }
