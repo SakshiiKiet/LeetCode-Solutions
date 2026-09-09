16eee0bdab99802d28a44ce4d380ebd6f87e8a66
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-09 10:57:47 +0530

    [509] [Time Beats: 100.00%] [Memory Beats: 99.93%] - LeetPush

--- 509-Fibonacci-Number.java
+++ 509-Fibonacci-Number.java
@@ -1,13 +1,13 @@
 class Solution {
-    
     public int fib(int n) {
-        int[]dp=new int[n+1];
-        if(n==0) return 0;
-        dp[0]=0;
-        dp[1]=1;
-       for(int i=2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
-       }
-       return dp[n];
+        if(n<=1) return n;
+        int prev=0;
+        int prev1=1;
+        for(int i=2;i<=n;i++){
+            int curr=prev+prev1;
+            prev=prev1;
+            prev1=curr;
+        }
+        return prev1;
     }
 }
