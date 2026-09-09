8f2b92d283819f033fda7bfe934a6e8c09661a68
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-09 11:09:49 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 17.32%] - LeetPush

--- 70-Climbing-Stairs.java
+++ 70-Climbing-Stairs.java
@@ -1,12 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp=new int[n+1];
-        //if(n==0) return 1;
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        if(n<=1) return 1;
+       int prev=1;
+       int prev1=1;
+       for(int i=2;i<=n;i++){
+        int curr=prev+prev1;
+        prev=prev1;
+        prev1=curr;
+       }
+       return prev1;
     }
 }
