aa2ad4f4a110fdad8818a8edd4507bfd7ad32c61
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-11 15:58:11 +0530

    [62] [Time Beats: 100.00%] [Memory Beats: 47.29%] - LeetPush

--- 62-Unique-Paths.java
+++ 62-Unique-Paths.java
@@ -1,18 +1,18 @@
 class Solution {
-    int[][]dp;
+    int[][] dp;
     public int uniquePaths(int m, int n) {
         dp=new int[m][n];
-    for(int[] rows:dp){
-        Arrays.fill(rows,-1);
-    }
+        for(int[] rows:dp){
+            Arrays.fill(rows,-1);
+        }
         return solve(m-1,n-1);
     }
     int solve(int i,int j){
         if(i==0 && j==0) return 1;
         if(i<0 || j<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int up=solve(i-1,j);
-        int left=solve(i,j-1);
-        return dp[i][j]=up+left;
+        int l=solve(i-1,j);
+        int r=solve(i,j-1);
+        return dp[i][j]=l+r;
     }
 }
