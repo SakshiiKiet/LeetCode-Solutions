6b283944d93d0c96754ad613a5b404f57563df59
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-15 10:42:08 +0530

    [64] [Time Beats: 9.51%] [Memory Beats: 72.35%] - LeetPush

--- 64-Minimum-Path-Sum.java
+++ 64-Minimum-Path-Sum.java
@@ -1,22 +1,21 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] dp = new int[m][n];
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(i==0 && j==0){
-                dp[i][j]=grid[0][0];
-            }else{
-                 int up = Integer.MAX_VALUE;
-                    int left = Integer.MAX_VALUE;
-
-                if (i > 0) up = grid[i][j] +dp[i - 1][j];
-                if (j > 0) left = grid[i][j] +dp[i][j - 1];
-                dp[i][j]=Math.min(up,left);
+        int m=grid.length;
+        int n=grid[0].length;
+        int[][]dp=new int[m][n];
+        dp[0][0]=grid[0][0];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                else {
+                    int up=Integer.MAX_VALUE;
+                    int l=Integer.MAX_VALUE;
+                    if(i>0) up=dp[i-1][j]+grid[i][j];
+                    if(j>0) l=dp[i][j-1]+grid[i][j];
+                    dp[i][j]=Math.min(up,l);
+                }
             }
         }
-       }
         return dp[m-1][n-1];
     }
 }
