3f0397c1924cd9ff3d89fda9440fb6435444d3f7
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-15 12:55:17 +0530

    [931] [Time Beats: 25.87%] [Memory Beats: 65.02%] - LeetPush

--- 931-Minimum-Falling-Path-Sum.java
+++ 931-Minimum-Falling-Path-Sum.java
@@ -1,28 +1,30 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int n=matrix.length;
-       int[][] dp=new int[n][n];
-      for(int j=0;j<n;j++){
-        dp[0][j]=matrix[0][j];
-      }
-      for(int i=1;i<n;i++){
-        for(int j=0;j<n;j++){
-        int p=dp[i-1][j]+matrix[i][j];
-        int q=Integer.MAX_VALUE;
-        int r=Integer.MAX_VALUE;
-        if(j-1>=0){
-          q=dp[i-1][j-1]+matrix[i][j];
-        }
-         if(j+1<n){
-          r=dp[i-1][j+1]+matrix[i][j];
+         int n= matrix[0].length;
+        int[] prev=new int[n];
+         for(int j=0;j<n;j++){
+            prev[j]=matrix[0][j];
          }
-         dp[i][j]=Math.min(p,Math.min(q,r));
+          for(int i=1;i<n;i++){
+            int[] curr=new int[n];
+            for(int j=0;j<n;j++){
+                int s= matrix[i][j]+prev[j];
+                int ld=Integer.MAX_VALUE;
+                int rd=Integer.MAX_VALUE;
+                if(j-1>=0){
+                 ld=matrix[i][j]+prev[j-1];
+                }
+                if(j+1<n){
+                    rd=matrix[i][j]+prev[j+1];
+                }
+                curr[j]=Math.min(s,Math.min(rd,ld));
+            }
+            prev=curr;
+          }
+         int min=prev[0];
+        for(int j=1;j<n;j++){
+           min=Math.min(min,prev[j]);
         }
-      }
-      int min=dp[n-1][0];
-      for(int j=1;j<n;j++){
-        min=Math.min(min,dp[n-1][j]);
-      }
-      return min;
+        return min;
     }
 }
