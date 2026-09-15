d0348ceccaf18f79b5f9e2212853b5a88770bfa4
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-15 09:53:25 +0530

    [63] [Time Beats: 100.00%] [Memory Beats: 64.61%] - LeetPush

--- 63-Unique-Paths-II.java
+++ 63-Unique-Paths-II.java
@@ -1,21 +1,28 @@
 class Solution {
-    int[][] dp;
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
-        dp=new int[m][n];
-        for(int[] rows:dp){
-            Arrays.fill(rows,-1);
+        int[] prev=new int[n];
+        prev[0]=0;
+        for(int i=0;i<m;i++){
+            int[] temp=new int[n];
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                    temp[j]=0;
+                }
+                 else if(i==0 && j==0){
+                 temp[j] = 1;
+                 }
+                else{
+                    int up=0;
+                    int l=0;
+                    if(i>0) up=prev[j];
+                    if(j>0) l=temp[j-1];
+                   temp[j]=up+l;
+                }
+            }
+            prev=temp;
         }
-        return solve(m-1,n-1,obstacleGrid);
-    }
-    int solve(int i,int j,int[][] obstacleGrid){
-        if(i>=0 && j>=0 && obstacleGrid[i][j]==1) return 0;
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up=solve(i-1,j, obstacleGrid);
-        int l=solve(i,j-1, obstacleGrid);
-        return dp[i][j]=up+l;
+        return prev[n-1];
     }
 }
