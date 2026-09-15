8014286244bfc5d7fe339c16ea1c6ec2a54d5c36
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-15 12:00:28 +0530

    [120] [Time Beats: 32.93%] [Memory Beats: 50.17%] - LeetPush

--- 120-Triangle.java
+++ 120-Triangle.java
@@ -1,17 +1,19 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
-        int[][] dp=new int[n][n];
+        int[] prev=new int[n];
         for(int j=0;j<n;j++){
-            dp[n-1][j]=triangle.get(n-1).get(j);
+            prev[j]=triangle.get(n-1).get(j);
         }
         for(int i=n-2;i>=0;i--){
+            int[] temp=new int[n];
             for(int j=i;j>=0;j--){
-                int u=dp[i+1][j]+triangle.get(i).get(j);
-                int l=dp[i+1][j+1]+triangle.get(i).get(j);
-                dp[i][j]=Math.min(u,l);
+              int u=prev[j]+triangle.get(i).get(j);
+              int l=prev[j+1]+triangle.get(i).get(j);
+              temp[j]=Math.min(u,l);
             }
+            prev=temp;
         }
-        return dp[0][0];
+        return prev[0];
     }
 }
