b366ef3c83587c251140821817a83acd2645c388
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-16 11:28:00 +0530

    [1463] [Time Beats: 62.52%] [Memory Beats: 76.52%] - LeetPush

--- 1463-Cherry-Pickup-II.java
+++ 1463-Cherry-Pickup-II.java
@@ -1,54 +1,38 @@
 class Solution {
+    int[][][]dp;
     public int cherryPickup(int[][] grid) {
-
-        int n = grid.length;
-        int m = grid[0].length;
-
-        int[][][] dp = new int[n][m][m];
-
-        // Base case: last row
-        for (int j1 = 0; j1 < m; j1++) {
-            for (int j2 = 0; j2 < m; j2++) {
-                if (j1 == j2) {
-                    dp[n - 1][j1][j2] = grid[n - 1][j1];
-                } else {
-                    dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+         int m=grid[0].length;
+         int n=grid.length;
+         dp=new int[n][m][m];
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                Arrays.fill(dp[i][j],-1);
+            }
+         }
+        return solve(0,0,m-1,grid);
+    }
+    int solve(int i,int j1,int j2,int[][] grid){
+        int n=grid.length;
+        int m=grid[0].length;
+        if(j1<0 || j1>=m || j2<0 || j2>=m) return Integer.MIN_VALUE/2;
+        if(i==n-1){
+            if(j1==j2){
+                return grid[i][j1];
+            }else{
+                return grid[i][j1]+grid[i][j2];
+            }
+        }
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+        int max=Integer.MIN_VALUE;
+        for(int dj1=-1;dj1<=1;dj1++){
+            for(int dj2=-1;dj2<=1;dj2++){
+                if(j1==j2){
+                    max=Math.max(max,grid[i][j1]+solve(i+1,j1+dj1,j2+dj2,grid));
+                }else{
+                    max=Math.max(max,grid[i][j1]+grid[i][j2]+solve(i+1,j1+dj1,j2+dj2,grid));
                 }
             }
         }
-
-        // Fill DP table bottom-up
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j1 = 0; j1 < m; j1++) {
-                for (int j2 = 0; j2 < m; j2++) {
-
-                    int max = Integer.MIN_VALUE;
-
-                    for (int dj1 = -1; dj1 <= 1; dj1++) {
-                        for (int dj2 = -1; dj2 <= 1; dj2++) {
-
-                            int nj1 = j1 + dj1;
-                            int nj2 = j2 + dj2;
-
-                            if (nj1 >= 0 && nj1 < m && nj2 >= 0 && nj2 < m) {
-
-                                int value;
-                                if (j1 == j2) {
-                                    value = grid[i][j1]+dp[i + 1][nj1][nj2];
-                                } else {
-                                    value = grid[i][j1] + grid[i][j2]+dp[i + 1][nj1][nj2];;
-                                }
-                                max = Math.max(max, value);
-                            }
-                        }
-                    }
-
-                    dp[i][j1][j2] = max;
-                }
-            }
-        }
-
-        // Starting positions: robot1 at 0, robot2 at m-1
-        return dp[0][0][m - 1];
+        return dp[i][j1][j2]=max;
     }
 }
