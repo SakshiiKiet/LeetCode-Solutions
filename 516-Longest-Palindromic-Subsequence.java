49beed94391ba33eb0a232b143bb45ad8571cf82
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-17 16:52:08 +0530

    [516] [Time Beats: 15.44%] [Memory Beats: 86.65%] - LeetPush

--- 516-Longest-Palindromic-Subsequence.java
+++ 516-Longest-Palindromic-Subsequence.java
@@ -1,22 +1,29 @@
 class Solution {
-    int[][] dp;
     public int longestPalindromeSubseq(String s) {
-    int n=s.length();
-        dp=new int[n+1][n+1];
-        for(int[] rows:dp){
-            Arrays.fill(rows,-1);
-        }
+        int n=s.length();
         String rev=new StringBuilder(s).reverse().toString();
-        return solve(s.length(),rev.length(),s,rev);
+        return longestCommonSubsequence(s,rev);
     }
-    int solve(int i,int j,String s1,String s2){
-        if(i==0 || j==0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i-1)==s2.charAt(j-1)){
-            dp[i][j]= 1+solve(i-1,j-1,s1,s2);
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n=text1.length();
+        int m=text2.length();
+        int[][] dp=new int[n+1][m+1];
+       for(int j=0;j<m;j++){
+        dp[0][j]=0;
+       }
+       for(int i=0;i<n;i++){
+        dp[i][0]=0;
+       }
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+          if(text1.charAt(i-1)==text2.charAt(j-1)){
+            dp[i][j]=1+dp[i-1][j-1];
         }else{
-            dp[i][j]= Math.max(solve(i-1,j,s1,s2),solve(i,j-1,s1,s2));
+             dp[i][j]=0+Math.max(dp[i-1][j],dp[i][j-1]);
         }
-       return dp[i][j];
+       
+        }
+       }
+       return dp[n][m];
     }
 }
