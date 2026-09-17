2f637c5e82213a8c9ac313b50cbf17dd2bb31463
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-17 13:36:47 +0530

    [518] [Time Beats: 24.73%] [Memory Beats: 8.26%] - LeetPush

--- 518-Coin-Change-II.java
+++ 518-Coin-Change-II.java
@@ -1,35 +1,27 @@
 class Solution {
+    int[][] dp;
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-
-        int[] prev = new int[amount + 1];
-
-        // Base case
-        for (int t = 0; t <= amount; t++) {
-            if (t % coins[0] == 0) {
-                prev[t] = 1;
-            } else {
-                prev[t] = 0;
+        int n=coins.length;
+        dp=new int[n][amount+1];
+        for(int[] rows:dp){
+            Arrays.fill(rows,-1);
+        }
+        return solve(n-1,amount,coins);
+    }
+    int solve(int i,int amount,int[] coins){
+        if(i==0){
+            if(amount%coins[i]==0){
+                return 1;
+            }else{
+                return 0;
             }
         }
-        for (int i = 1; i < n; i++) {
-            int[] curr = new int[amount + 1];
-
-            for (int j = 0; j <= amount; j++) {
-                int notTake = prev[j];
-                int take = 0;
-
-                if (j >= coins[i]) {
-                    take = curr[j - coins[i]]; 
-                }
-
-                curr[j] = take + notTake;
-            }
-
-            prev = curr; 
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int not=solve(i-1,amount,coins);
+        int take=0;
+        if(amount>=coins[i]){
+            take=solve(i,amount-coins[i],coins);
         }
-
-        return prev[amount];
+        return dp[i][amount]=take+not;
     }
 }
-    
