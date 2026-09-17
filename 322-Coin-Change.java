ad12b4919a9001b89cf99d17f2f9bdd2d6ad7087
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-17 13:20:24 +0530

    [322] [Time Beats: 83.79%] [Memory Beats: 25.99%] - LeetPush

--- 322-Coin-Change.java
+++ 322-Coin-Change.java
@@ -1,28 +1,30 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        int[][] dp=new int[n][amount+1];
+        int[] prev=new int[amount+1];
         
        for(int j=0;j<=amount;j++){
           if(j%coins[0]==0){
-                dp[0][j]= j/coins[0];
+                prev[j]= j/coins[0];
             }else{
-             dp[0][j]=Integer.MAX_VALUE/2;
+             prev[j]=Integer.MAX_VALUE/2;
             }
        }
         if(amount==0) return 0;
         for(int i=1;i<n;i++){
+            int[] temp=new int[amount+1];
             for(int j=0;j<=amount;j++){
-              int not=dp[i-1][j];
+              int not=prev[j];
         int take=Integer.MAX_VALUE/2;
         if(j>=coins[i]){
-            take=1+dp[i][j-coins[i]];
+            take=1+temp[j-coins[i]];
         }
-        dp[i][j]=Math.min(take,not);
+       temp[j]=Math.min(take,not);
             }
+            prev=temp;
         }
         
-      int ans = dp[n-1][amount];
+      int ans = prev[amount];
 
         return ans >= Integer.MAX_VALUE / 2 ? -1 : ans;
     }
