4b2deaf4f4a55f4e4fa5560914ca1ff31559eb95
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-17 13:17:36 +0530

    [322] [Time Beats: 83.79%] [Memory Beats: 38.57%] - LeetPush

--- 322-Coin-Change.java
+++ 322-Coin-Change.java
@@ -1,28 +1,29 @@
 class Solution {
-    int[][] dp;
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        dp=new int[n][amount+1];
-        for(int[] rows:dp){
-            Arrays.fill(rows,-1);
-        }
-       int ans= solve(n-1,amount,coins);
-       return ans>=Integer.MAX_VALUE/2?-1:ans;
-    }
-    int solve(int i,int amount,int[] coins){
-        if(i==0){
-            if(amount%coins[i]==0){
-                return amount/coins[i];
+        int[][] dp=new int[n][amount+1];
+        
+       for(int j=0;j<=amount;j++){
+          if(j%coins[0]==0){
+                dp[0][j]= j/coins[0];
+            }else{
+             dp[0][j]=Integer.MAX_VALUE/2;
             }
-            return Integer.MAX_VALUE/2;
-        }
+       }
         if(amount==0) return 0;
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        int not=solve(i-1,amount,coins);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+              int not=dp[i-1][j];
         int take=Integer.MAX_VALUE/2;
-        if(amount>=coins[i]){
-            take=1+solve(i,amount-coins[i],coins);
+        if(j>=coins[i]){
+            take=1+dp[i][j-coins[i]];
         }
-        return dp[i][amount]= Math.min(not,take);
+        dp[i][j]=Math.min(take,not);
+            }
+        }
+        
+      int ans = dp[n-1][amount];
+
+        return ans >= Integer.MAX_VALUE / 2 ? -1 : ans;
     }
 }
