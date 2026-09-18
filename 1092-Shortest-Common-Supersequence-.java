22e32c4da7433270e3540c4192173be602b4f622
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-18 05:35:12 +0530

    [1092] [Time Beats: 81.16%] [Memory Beats: 36.76%] - LeetPush

--- 1092-Shortest-Common-Supersequence-.java
+++ 1092-Shortest-Common-Supersequence-.java
@@ -1,38 +1,39 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
-        int m=str1.length();
-        int n=str2.length();
-        int[][] dp=new int[m+1][n+1];
-        for(int j=0;j<=n;j++){
-            dp[0][j]=0;
+        int n=str1.length();
+        int m=str2.length();
+        int[][] dp=new int[n+1][m+1];
+       for(int j=0;j<m;j++){
+        dp[0][j]=0;
+       }
+       for(int i=0;i<n;i++){
+        dp[i][0]=0;
+       }
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+          if(str1.charAt(i-1)==str2.charAt(j-1)){
+            dp[i][j]=1+dp[i-1][j-1];
+        }else{
+             dp[i][j]=0+Math.max(dp[i-1][j],dp[i][j-1]);
         }
-        for(int i=0;i<=m;i++){
-            dp[i][0]=0;
+       
         }
-        for(int i=1;i<=m;i++){
-         for(int j=1;j<=n;j++){
-            if(str1.charAt(i-1)==str2.charAt(j-1)){
-                dp[i][j]=1+dp[i-1][j-1];
-            }else{
-                dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-            }
-         }
+       }
+       StringBuilder ans=new StringBuilder();
+       int i=n; int j=m;
+       while(i>0 && j>0){
+        if(str1.charAt(i-1)==str2.charAt(j-1)){
+            ans.append(str1.charAt(i-1));
+            i--;
+            j--;
+        }else if(dp[i-1][j]>dp[i][j-1]){
+            ans.append(str1.charAt(i-1));
+            i--;
+        }else{
+            ans.append(str2.charAt(j-1));
+            j--;
         }
-        StringBuilder ans=new StringBuilder();
-        int i=m,j=n;
-        while(i>0 && j>0){
-            if(str1.charAt(i-1)==str2.charAt(j-1)){
-             ans.append(str1.charAt(i-1));
-             i--;
-             j--;
-            }else if(dp[i-1][j]>dp[i][j-1]){
-                ans.append(str1.charAt(i-1));
-                i--;
-            }else{
-                ans.append(str2.charAt(j-1));
-                j--;
-            }
-        }
+       }
         while(i>0){
         ans.append(str1.charAt(i-1));
         i--;
