431ea382d27cb4f1a292ce4eb22c5430e07a83dc
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-18 13:59:33 +0530

    [309] [Time Beats: -%] [Memory Beats: -%] - LeetPush

--- 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.java
+++ 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.java
@@ -1,24 +1,21 @@
 class Solution {
-    int[][]dp;
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        dp=new int[n][2];
-        for(int[] rows:dp){
-            Arrays.fill(rows,-1);
-        }
-       return profit(0,1,prices);
-    }
-    int profit(int i,int buy,int[] prices){
-        if(i>=prices.length) return 0;
-        int profit=0;
-        if(dp[i][buy]!=-1) return dp[i][buy];
+      int[][]  dp=new int[n+2][2];
+       
+        dp[n][0]=dp[n][1]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int buy=1;buy>=0;buy--){
+                 int profit=0;
         if(buy==1){
-        profit=Math.max((-prices[i]+profit(i+1,0,prices)), //buy
-                        (0+profit(i+1,1,prices)));          //not buy       
+        dp[i][buy]=Math.max((-prices[i]+dp[i+1][0]), //buy
+                        (0+dp[i+1][1]));          //not buy       
      }else{
-        profit=Math.max((prices[i]+profit(i+2,1,prices)), //sell
-                        (0+profit(i+1,0,prices)));          //not sell      
-     }
-        return dp[i][buy]= profit;
+        dp[i][buy]=Math.max((prices[i]+dp[i+2][1]), //sell
+                        (0+dp[i+1][0]));          //not sell      
+              }
+            }
+        }
+        return dp[0][1];
     }
 }
