e03aad941d51cda86a7858838e594664ad82621e
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-18 12:15:18 +0530

    [122] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

--- 122-Best-Time-to-Buy-and-Sell-Stock-II.java
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II.java
@@ -1,25 +1,21 @@
 class Solution {
-    int[][] dp;
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        dp=new int[n][2];
-        for(int[] rows:dp ){
-            Arrays.fill(rows,-1);
-        }
-        return solve(0,1,prices);
-    }
-    int solve(int i,int buy,int[] prices){
-        int n=prices.length;
-        if(i==n){
-            return 0;
-        }
-        if(dp[i][buy]!=-1) return dp[i][buy];
-        if(buy==1){
-            return dp[i][buy]= Math.max(-prices[i]+solve(i+1,0,prices),
-                            0+solve(i+1,1,prices));
+        int[][]dp=new int[n+1][2];
+        dp[n][0]=0;
+        dp[n][1]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+              if(buy==1){
+             dp[i][buy]= Math.max((-prices[i]+dp[i+1][0]),
+                            0+dp[i+1][1]);
         }else{
-           return dp[i][buy]=Math.max(prices[i]+solve(i+1,1,prices),
-                           0+solve(i+1,0,prices));
+            dp[i][buy]=Math.max(prices[i]+dp[i+1][1],
+                           0+dp[i+1][0]);
+            }
+            
         }
+    }
+        return dp[0][1];
     }
 }
