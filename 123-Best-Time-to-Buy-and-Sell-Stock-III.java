ce3f22a4c9b7c95a4b0266c8b3db38ae64a6ddf4
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-18 13:30:00 +0530

    [123] [Time Beats: 47.42%] [Memory Beats: 43.60%] - LeetPush

--- 123-Best-Time-to-Buy-and-Sell-Stock-III.java
+++ 123-Best-Time-to-Buy-and-Sell-Stock-III.java
@@ -1,27 +1,23 @@
 class Solution {
-    int[][][]dp;
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        dp=new int[n][2][3];
-         for(int i = 0; i < n; i++){
-    for(int j = 0; j < 2; j++){
-        Arrays.fill(dp[i][j], -1);
-    }
-}
-       return profit(0,1,2,prices);
-    }
-    int profit(int i,int buy,int cap,int[] prices){
-        if(cap==0) return 0;
-        if(i==prices.length) return 0;
-        if(dp[i][buy][cap]!=-1) return dp[i][buy][cap];
-        int profit=0;
-        if(buy==1){
-        profit=Math.max((-prices[i]+profit(i+1,0,cap,prices)), //buy
-                        (0+profit(i+1,1,cap,prices)));          //not buy       
-     }else{
-        profit=Math.max((prices[i]+profit(i+1,1,cap-1,prices)), //sell
-                        (0+profit(i+1,0,cap,prices)));          //not sell      
-     }
-        return dp[i][buy][cap]=profit;
+        int n = prices.length;
+        int[][] dp=new int[n+1][4+1];
+        for(int trans=0;trans<4;trans++){
+            dp[n][trans]=0;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][4]=0;
+        }
+        for(int i=n-1;i>=0;i--){
+          for(int trans=3;trans>=0;trans--){
+             if(trans%2==0){ //buy
+          dp[i][trans]=Math.max(-prices[i]+dp[i+1][trans+1],0+dp[i+1][trans]);
+        }else{
+           dp[i][trans]=Math.max(prices[i]+dp[i+1][trans+1],0+dp[i+1][trans]) ;
+        }
+          }
+        }
+       
+        return dp[0][0];
     }
 }
