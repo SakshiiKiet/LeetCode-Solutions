9a2119d71152c9e052df051118fb333cb4dd8769
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-18 07:12:55 +0530

    [72] [Time Beats: 98.09%] [Memory Beats: 35.01%] - LeetPush

--- 72-Edit-Distance.java
+++ 72-Edit-Distance.java
@@ -1,25 +1,21 @@
 class Solution {
+    int[][] dp;
     public int minDistance(String word1, String word2) {
          int n=word1.length();
          int m=word2.length();
-         int[] prev=new int[m+1];
-         for(int j=0;j<=m;j++){
-            prev[j]=j;
+         dp=new int[n][m];
+         for(int[] rows:dp){
+            Arrays.fill(rows,-1);
          }
-        
-        for(int i=1;i<=n;i++){
-            int[] curr=new int[m+1];
-            curr[0]=i;
-            for(int j=1;j<=m;j++){
-               if(word1.charAt(i-1)==word2.charAt(j-1)){
-                 curr[j]=0 + prev[j-1];
-               } 
-               else{
-                 curr[j]=1+Math.min(curr[j-1],Math.min(prev[j],prev[j-1]));
-               }  
-            }
-            prev=curr;
-        }
-       return prev[m];
+        return operations(n-1,m-1,word1,word2);
+    }
+    int operations(int i,int j,String s1, String s2){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1.charAt(i)==s2.charAt(j)){
+          return dp[i][j]=0 + operations(i-1,j-1,s1,s2);
+        } 
+        return dp[i][j]=1+Math.min(operations(i,j-1,s1,s2),Math.min(operations(i-1,j,s1,s2),operations(i-1,j-1,s1,s2)));
     }
 }
