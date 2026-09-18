23f94cdae160f7e378ca50bfed970e24be03cd1f
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-18 07:14:22 +0530

    [72] [Time Beats: 65.82%] [Memory Beats: 70.44%] - LeetPush

--- 72-Edit-Distance.java
+++ 72-Edit-Distance.java
@@ -1,21 +1,25 @@
 class Solution {
-    int[][] dp;
     public int minDistance(String word1, String word2) {
          int n=word1.length();
          int m=word2.length();
-         dp=new int[n][m];
-         for(int[] rows:dp){
-            Arrays.fill(rows,-1);
-         }
-        return operations(n-1,m-1,word1,word2);
-    }
-    int operations(int i,int j,String s1, String s2){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)){
-          return dp[i][j]=0 + operations(i-1,j-1,s1,s2);
-        } 
-        return dp[i][j]=1+Math.min(operations(i,j-1,s1,s2),Math.min(operations(i-1,j,s1,s2),operations(i-1,j-1,s1,s2)));
+         int[][] dp=new int[n+1][m+1];
+         
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+               if(word1.charAt(i-1)==word2.charAt(j-1)){
+                 dp[i][j]=0 + dp[i-1][j-1];
+               } 
+               else{
+                 dp[i][j]=1+Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));
+               }  
+            }
+        }
+       return dp[n][m];
     }
 }
