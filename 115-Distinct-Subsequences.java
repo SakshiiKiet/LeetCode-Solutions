d181eacc3e2e6d97309d9e7278e7c06df93824ad
Sakshi Maan <sakshimaan2004@gmail.com>
2026-09-18 05:53:49 +0530

    [115] [Time Beats: 61.47%] [Memory Beats: 33.94%] - LeetPush

--- 115-Distinct-Subsequences.java
+++ 115-Distinct-Subsequences.java
@@ -1,22 +1,25 @@
 class Solution {
-    int[][] dp;
     public int numDistinct(String s, String t) {
         int n=s.length();
         int m=t.length();
-        dp=new int[n+1][m+1];
-        for(int[] rows:dp){
-            Arrays.fill(rows,-1);
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }                          //Base Case    
+        for(int j=1;j<=m;j++){ //j starts from 1 becoz in previous loop u already put at j=0==1
+            dp[0][j]=0;
         }
-        return solve(n,m,s,t);
-    }
-    int solve(int i,int j,String s, String t){
-        if(j==0) return 1;
-        if(i==0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
         if(s.charAt(i-1)==t.charAt(j-1)){
-            return dp[i][j]=solve(i-1,j-1,s,t)+solve(i-1,j,s,t);
+            dp[i][j]= dp[i-1][j-1]+dp[i-1][j];
         }else{
-            return dp[i][j]=solve(i-1,j,s,t);
+           dp[i][j]= dp[i-1][j];  
         }
+         }
+        }
+        return dp[n][m];
     }
+    
 }
